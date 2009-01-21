9de8ac9f89bca58a167d8a4abb6a28567f789862
dvd <dvd@gnx.it>
2009-01-21 10:31:37 +0100

    elenco dei post del microblog basato su generic views

---
+++ microblog/templatetags/microblog.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from django import template
+from microblog import models
+
+register = template.Library()
+
+@register.inclusion_tag('microblog/show_post_summary.html', takes_context=True)
+def show_post_summary(context, post):
+    lang = context['LANGUAGE_CODE']
+    contents = dict((c.language, c) for c in post.postcontent_set.all())
+    try:
+        content = contents[lang]
+        if not content.headline:
+            raise KeyError()
+    except KeyError:
+        for l, c in contents.items():
+            if c.headline:
+                content = c
+                break
+        else:
+            raise ValueError('There is no a valid content (in any language)')
+    return {
+        'post': post,
+        'content': content
+    }
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,8 +1,14 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import *
-from microblog import views
+from microblog import views, models
 
 urlpatterns = patterns('',
+    (
+        r'^$', 'django.views.generic.list_detail.object_list',
+        {
+            'queryset': models.Post.objects.all()
+        }
+    ),
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\w{1,2})/(?P<slug>[^/]+)/?$', views.post),
 )
 
