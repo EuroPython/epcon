4484fa67a050baff7d798fd1f128ee5f8b38f7ad
dvd <dvd@gnx.it>
2009-01-22 23:29:53 +0100

    nuovi ADMIN

--- settings.py
+++ settings.py
@@ -4,8 +4,11 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('dvd', 'dvd@gnx.it.com'),
+    ('dvd', 'dvd@gnx.it'),
+    ('c8e', 'carlo.miron@gmail.com'),
+    ('manlio', 'manlio.perillo@gmail.com')
 )
+
 
 MANAGERS = ADMINS
 
