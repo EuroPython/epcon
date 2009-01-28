5b27694fa11214eaa073cacd4870550af284abc1
dvd <dvd@gnx.it>
2009-01-28 16:49:34 +0100

    supporto per il box download

--- p3/templatetags/p3.py
+++ p3/templatetags/p3.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
 from __future__ import absolute_import
+import mimetypes
+import os
+import os.path
 import re
 from django import template
 from django.conf import settings
@@ -7,6 +10,8 @@ from django.conf import settings
 from p3 import models
 from pages import models as PagesModels
 from microblog import models as BlogModels
+
+mimetypes.init()
 
 register = template.Library()
 
@@ -97,3 +102,29 @@ def box_pycon_italia():
 @register.inclusion_tag('p3/box_newsletter.html')
 def box_newsletter():
     return {}
+
+@register.inclusion_tag('p3/box_download.html')
+def box_download(fname, label=None):
+    if '..' in fname:
+        raise template.TemplateSyntaxError("file path cannot contains ..")
+    if fname.startswith('/'):
+        raise template.TemplateSyntaxError("file path cannot starts with /")
+    if label is None:
+        label = os.path.basename(fname)
+    try:
+        fpath = os.path.join(settings.P3_STUFF_DIR, fname)
+        print fpath
+        stat = os.stat(fpath)
+    except (AttributeError, OSError), e:
+        print e
+        fsize = ftype = None
+    else:
+        fsize = stat.st_size
+        ftype = mimetypes.guess_type(fpath)[0]
+        
+    return {
+        'url': settings.MEDIA_URL + 'p3/stuff/' + fname,
+        'label': label,
+        'fsize': fsize,
+        'ftype': ftype,
+    }
