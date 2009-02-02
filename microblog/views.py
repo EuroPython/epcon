1425d07d581d3a9ffec5ea3a4b70699febcefb37
dvd <dvd@gnx.it>
2009-02-02 14:08:25 +0100

    sollevo un 404 se il post on esiste

--- microblog/views.py
+++ microblog/views.py
@@ -2,10 +2,11 @@ import datetime
 from microblog import models
 
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def post_detail(request, year, month, day, slug):
-    postcontent = models.PostContent.objects.get(
+    postcontent = get_object_or_404(
+        models.PostContent, 
         slug = slug, 
         post__date__year = int(year),
         post__date__month = int(month),
