25bfac1fc070704b4360d7b82a54851524458db6
dvd <dvd@gnx.it>
2009-02-11 20:58:25 +0100

    view preliminare per lo speaker

--- conference/views.py
+++ conference/views.py
@@ -1,2 +1,10 @@
 # -*- coding: UTF-8 -*-
+from conference import models
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
 
+def speaker(request, slug):
+    spk = get_object_or_404(models.Speaker, slug = slug)
+    return render_to_response(
+        'conference/speaker.html', { 'speaker': spk },
+        context_instance = RequestContext(request))
