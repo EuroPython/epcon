115b5f3e61a7567e082c1a5bc9eefda0958be1aa
dvd <dvd@gnx.it>
2009-03-02 22:13:27 +0100

    supporto per il genro wrapper

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 # -*- coding: UTF-8 -*-
+import urllib
 from conference import models
+from django.conf import settings
+from django.http import Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 
@@ -14,3 +17,17 @@ def talk(request, slug):
     return render_to_response(
         'conference/talk.html', { 'talk': tlk },
         context_instance = RequestContext(request))
+
+def genro_wrapper(request):
+    """
+    mostra in un iframe l'applicazione conference di genropy
+    """
+    try:
+        conf = dict(settings.GNR_CONFERENCE)
+    except AttributeError:
+        raise Http404()
+    conf['src'] += '?' + urllib.urlencode(request.GET)
+    return render_to_response(
+        'conference/genro_wrapper.html', conf,
+        context_instance = RequestContext(request))
+
