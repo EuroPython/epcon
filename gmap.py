eb9033eb48fc839ef01a47250886358343b0eb4d
dvd <dvd@develer.com>
2009-04-07 10:40:02 +0200

    funzione di geocoding che interroga google maps

---
+++ gmap.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+import urllib
+import httplib
+import simplejson
+
+G = 'maps.google.com'
+
+def geocode(address, key, country):
+    """
+    interroga google maps e ritorna le coordinate geografiche
+    dell'indirizzo passato
+    """
+    # see http://code.google.com/intl/it/apis/maps/documentation/geocoding/#GeocodingRequests
+    params = {
+        'q': address,
+        'key': key,
+        'sensor': 'false',
+        'output': 'json',
+        'oe': 'utf8',
+    }
+    if country:
+        params['gl'] = country
+
+    url = '/maps/geo?' + urllib.urlencode(params.items())
+    conn = httplib.HTTPConnection(G)
+    try:
+        conn.request('GET', url)
+        r = conn.getresponse()
+        if r.status == 200:
+            return simplejson.loads(r.read())
+        else:
+            return {'Status': {'code': r.status }}
+    finally:
+        conn.close()
