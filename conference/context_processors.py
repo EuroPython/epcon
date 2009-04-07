8f65a6f117085cf90aa36649282ee78545f48b2c
dvd <dvd@develer.com>
2009-04-07 14:48:09 +0200

    espongo l'api key di google map nel context

--- conference/context_processors.py
+++ conference/context_processors.py
@@ -5,10 +5,15 @@ def stuff(request):
     Aggiunge le variabili relative alla STUFF directory
     """
     stuff = '%sstuff/' % settings.MEDIA_URL
-    return {
+    ctx = {
         'STUFF_URL': stuff,
         'SPONSOR_LOGO_URL': '%ssponsor/' % stuff,
         'MEDIAPARTNER_LOGO_URL': '%smedia-partner/' % stuff,
         'SPEAKER_FACE_URL': '%sspeaker/' % stuff,
         'SLIDE_FILE_URL': '%sslides/' % stuff,
     }
+    try:
+        ctx['GOOGLE_MAPS_KEY'] = settings.GOOGLE_MAPS_CONFERENCE['key']
+    except (AttributeError, KeyError):
+        pass
+    return ctx
