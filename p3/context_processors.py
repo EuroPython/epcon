794245481616999b5a959cbbae913f05be7e9978
dvd <dvd@develer.com>
2009-04-08 17:14:30 +0200

    messaggio higlight modificato

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -1,9 +1,20 @@
-from django.conf import settings
+from datetime import date
 
 def highlight(request):
     """
     Aggiunge il messaggio da mostrare in tutte le pagine
     """
-    return {
-        'HIGHLIGHT': '<a href="/pycon3/registrazione/">Mancano solo <strong>10 giorni</strong> alla fine dell\'early bird</a>',
-    }
+    deadline = date(year = 2009, month = 4, day = 13)
+    t = deadline - date.today()
+
+    if t.days > 0:
+        msg = "Mancano solo *%d giorni* alla fine dell'Early Bird."
+        msg = (msg % t.days).replace('*', '<strong>', 1).replace('*', '</strong>')
+    elif t.days == 0:
+        msg = "L'Early Bird finisce oggi!"
+    else:
+        msg = None
+        
+    if msg:
+        msg = '<a href="/2009/registrazione/">' + msg + '</a>'
+    return { 'HIGHLIGHT': msg }
