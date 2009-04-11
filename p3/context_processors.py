baa7a4d4cd7c14345aef13d61b9a532eeb5aa73b
dvd <dvd@gnx.it>
2009-04-11 22:40:50 +0200

    ritocchi ai messaggi highlight

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -8,14 +8,17 @@ def highlight(request):
     deadline = date(year = 2009, month = 4, day = 13)
     t = deadline - date.today()
 
-    if t.days > 0:
+    if t.days > 1:
         msg = _("Mancano solo *%(days)d giorni* alla fine dell'Early Bird.")
-        msg = (msg % {'days': t.days}).replace('*', '<strong>', 1).replace('*', '</strong>')
+        msg = (msg % {'days': t.days})
+    elif t.days == 1:
+        msg = _("L'Early Bird finisce *domani*!")
     elif t.days == 0:
-        msg = _("L'Early Bird finisce oggi!")
+        msg = _("L'Early Bird finisce *oggi*!")
     else:
         msg = None
         
     if msg:
+        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
         msg = '<a href="/2009/registrazione/">' + msg + '</a>'
     return { 'HIGHLIGHT': msg }
