d9191f401517d8f600713d24a29d6e1ff429d8df
dvd <dvd@develer.com>
2009-04-15 16:18:48 +0200

    aggiornati i messaggi higlight

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -1,24 +1,34 @@
 from datetime import date
+from conference import assopy
 from django.utils.translation import ugettext_lazy as _
 
 def highlight(request):
     """
     Aggiunge il messaggio da mostrare in tutte le pagine
     """
-    deadline = date(year = 2009, month = 4, day = 13)
+    deadline = date(year = 2009, month = 5, day = 4)
     t = deadline - date.today()
 
     if t.days > 1:
-        msg = _("Mancano solo *%(days)d giorni* alla fine dell'Early Bird.")
-        msg = (msg % {'days': t.days})
+        msg = _("Mancano *%(days)d giorni* alla fine del Late Bird.")
+        msg = msg % {'days': t.days}
     elif t.days == 1:
-        msg = _("L'Early Bird finisce *domani*!")
+        msg = _("Il Late Bird finisce *domani*!")
     elif t.days == 0:
-        msg = _("L'Early Bird finisce *oggi*!")
+        msg = _("Il Late Bird finisce *oggi*!")
     else:
         msg = None
         
     if msg:
         msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
-        msg = '<a href="/2009/registrazione/">' + msg + '</a>'
+        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
+    else:
+        msg = ''
+
+    attendee = assopy.attendeeCount()
+    if attendee > 0:
+        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
+        m2 = m2 % {'seat': 420 - attendee} 
+        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
+        msg += ' ' + m2
     return { 'HIGHLIGHT': msg }
