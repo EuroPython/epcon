e12bcf1a1729e9be5f6d0dc0e0bc22c564475351
dvd <dvd@develer.com>
2009-04-15 14:23:39 +0200

    supporto per i doppi speaker

--- utils/dump-assopy-talks.py
+++ utils/dump-assopy-talks.py
@@ -30,17 +30,19 @@ def list_talk():
 
 def extract_talk(id):
     qtalk = """
-        SELECT t.title, t.duration, t.language, t.abstract, t.abstract_en, t.subject, t.main_speaker_id, e.code
+        SELECT t.title, t.duration, t.language, t.abstract, t.abstract_en, t.subject, t.main_speaker_id, e.code, ss.speaker_id
         FROM conference.conference_talk t INNER JOIN conference.conference_track tt
             ON t.track_id = tt.id
         INNER JOIN conference.conference_event e
             ON tt.event_id = e.id
+        LEFT JOIN conference.conference_secondary_speaker ss
+            ON t.id = ss.talk_id
         WHERE t.id=%s
     """
     qspk = """
         SELECT crd.first_name, crd.www, spk.activity, spk.area,
             crd.city, crd.state, spk.resume, spk.resume_en, crd.last_name, crd.card_name,
-            usr.username
+            usr.username, usr.firstname, usr.lastname
         FROM conference.conference_speaker spk INNER JOIN conference.conference_card crd
             ON spk.card_id = crd.id
         INNER JOIN adm.adm_user usr
@@ -59,25 +61,33 @@ def extract_talk(id):
         ET.SubElement(T, 'abstract', {'lang': 'it'}).text = enc(talk[3])
         ET.SubElement(T, 'abstract', {'lang': 'en'}).text = enc(talk[4])
         ET.SubElement(T, 'tags').text = enc(talk[5])
-        cursor.execute(qspk, (talk[6],))
-        speaker = cursor.fetchall()[0]
-        S = ET.SubElement(T, 'speaker')
-        name = '' if speaker[0] is None else speaker[0]
-        if speaker[8] is not None:
-            name += ' ' + speaker[8]
-        if not name:
-            name = speaker[9]
-        _speakers.append((speaker[10], name))
-        ET.SubElement(S, 'name').text = enc(name)
-        ET.SubElement(S, 'homepage').text = enc(speaker[1])
-        ET.SubElement(S, 'activity').text = enc(speaker[2])
-        ET.SubElement(S, 'industry').text = enc(speaker[3])
-        if speaker[5]:
-            ET.SubElement(S, 'location').text = enc(speaker[4]) + '/' + enc(speaker[5])
-        else:
-            ET.SubElement(S, 'location').text = enc(speaker[4])
-        ET.SubElement(S, 'bio', {'lang': 'it'}).text = enc(speaker[6])
-        ET.SubElement(S, 'bio', {'lang': 'en'}).text = enc(speaker[7])
+        def merge(*args):
+            args = filter(None, args)
+            x = ' '.join(args)
+            return x.strip()
+
+        for sid in (talk[6], talk[8]):
+            if not sid:
+                continue
+            cursor.execute(qspk, (sid, ))
+            speaker = cursor.fetchall()[0]
+            S = ET.SubElement(T, 'speaker')
+            name = merge(speaker[0], speaker[8])
+            if not name:
+                name = merge(speaker[11], speaker[12])
+            if not name:
+                name = speaker[9]
+            _speakers.append((speaker[10], name))
+            ET.SubElement(S, 'name').text = enc(name)
+            ET.SubElement(S, 'homepage').text = enc(speaker[1])
+            ET.SubElement(S, 'activity').text = enc(speaker[2])
+            ET.SubElement(S, 'industry').text = enc(speaker[3])
+            if speaker[5]:
+                ET.SubElement(S, 'location').text = enc(speaker[4]) + '/' + enc(speaker[5])
+            else:
+                ET.SubElement(S, 'location').text = enc(speaker[4])
+            ET.SubElement(S, 'bio', {'lang': 'it'}).text = enc(speaker[6])
+            ET.SubElement(S, 'bio', {'lang': 'en'}).text = enc(speaker[7])
 
     return T
 conn = psycopg2.connect(host=HOST, database=DBNAME, user=USER, password=PASSWORD)
