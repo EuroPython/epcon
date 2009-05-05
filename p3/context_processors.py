01b61f670fa1fa624bc4dbc48916f7a7f924fffd
dvd <dvd@gnx.it>
2009-05-05 21:51:32 +0200

    riabilitato il messaggio di highlight

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from datetime import date
 from conference import assopy
 from django.utils.translation import ugettext_lazy as _
@@ -6,31 +7,30 @@ def highlight(request):
     """
     Aggiunge il messaggio da mostrare in tutte le pagine
     """
-    return { 'HIGHLIGHT': None }
-#    deadline = date(year = 2009, month = 5, day = 4)
-#    t = deadline - date.today()
-#
-#    if t.days > 1:
-#        msg = _("Mancano *%(days)d giorni* alla fine del Late Bird.")
-#        msg = msg % {'days': t.days}
-#    elif t.days == 1:
-#        msg = _("Il Late Bird finisce *domani*!")
-#    elif t.days == 0:
-#        msg = _("Il Late Bird finisce *oggi*!")
-#    else:
-#        msg = None
-#        
-#    if msg:
-#        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
-#    else:
-#        msg = ''
-#
-#    attendee = assopy.attendeeCount()
-#    if attendee > 0:
-#        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
-#        m2 = m2 % {'seat': 420 - attendee} 
-#        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
-#        msg += ' ' + m2
-#    if msg:
-#        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
-#    return { 'HIGHLIGHT': msg }
+    deadline = date(year = 2009, month = 5, day = 8)
+    t = deadline - date.today()
+
+    if t.days > 1:
+        msg = _("Mancano *%(days)d giorni* all'inizio della conferenza.")
+        msg = msg % {'days': t.days}
+    elif t.days == 1:
+        msg = _("La conferenza inizia *domani*!")
+    elif t.days in (0, -1, -2):
+        return _("La conferenza è in fase di svolgimento!")
+    else:
+        return { 'HIGHLIGHT': None }
+        
+    if msg:
+        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
+    else:
+        msg = ''
+
+    attendee = assopy.attendeeCount()
+    if attendee > 0:
+        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
+        m2 = m2 % {'seat': 420 - attendee} 
+        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
+        msg += ' ' + m2
+    if msg:
+        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
+    return { 'HIGHLIGHT': msg }
