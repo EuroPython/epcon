03e860f3f11903cb8404554136847a92f9040737
dvd <dvd@develer.com>
2009-05-05 11:18:41 +0200

    rimosso l'highlight

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -6,30 +6,31 @@ def highlight(request):
     """
     Aggiunge il messaggio da mostrare in tutte le pagine
     """
-    deadline = date(year = 2009, month = 5, day = 4)
-    t = deadline - date.today()
-
-    if t.days > 1:
-        msg = _("Mancano *%(days)d giorni* alla fine del Late Bird.")
-        msg = msg % {'days': t.days}
-    elif t.days == 1:
-        msg = _("Il Late Bird finisce *domani*!")
-    elif t.days == 0:
-        msg = _("Il Late Bird finisce *oggi*!")
-    else:
-        msg = None
-        
-    if msg:
-        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
-    else:
-        msg = ''
-
-    attendee = assopy.attendeeCount()
-    if attendee > 0:
-        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
-        m2 = m2 % {'seat': 420 - attendee} 
-        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
-        msg += ' ' + m2
-    if msg:
-        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
-    return { 'HIGHLIGHT': msg }
+    return { 'HIGHLIGHT': None }
+#    deadline = date(year = 2009, month = 5, day = 4)
+#    t = deadline - date.today()
+#
+#    if t.days > 1:
+#        msg = _("Mancano *%(days)d giorni* alla fine del Late Bird.")
+#        msg = msg % {'days': t.days}
+#    elif t.days == 1:
+#        msg = _("Il Late Bird finisce *domani*!")
+#    elif t.days == 0:
+#        msg = _("Il Late Bird finisce *oggi*!")
+#    else:
+#        msg = None
+#        
+#    if msg:
+#        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
+#    else:
+#        msg = ''
+#
+#    attendee = assopy.attendeeCount()
+#    if attendee > 0:
+#        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
+#        m2 = m2 % {'seat': 420 - attendee} 
+#        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
+#        msg += ' ' + m2
+#    if msg:
+#        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
+#    return { 'HIGHLIGHT': msg }
