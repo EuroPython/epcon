8e716f7df67bfe6685a56d72e42eaaf2d07fd4b6
dvd <dvd@gnx.it>
2009-05-11 17:56:46 +0200

    disabilitati gli highlight

--- p3/context_processors.py
+++ p3/context_processors.py
@@ -7,30 +7,31 @@ def highlight(request):
     """
     Aggiunge il messaggio da mostrare in tutte le pagine
     """
-    deadline = date(year = 2009, month = 5, day = 8)
-    t = deadline - date.today()
-
-    if t.days > 1:
-        msg = _("Mancano *%(days)d giorni* all'inizio della conferenza.")
-        msg = msg % {'days': t.days}
-    elif t.days == 1:
-        msg = _("La conferenza inizia *domani*!")
-    elif t.days in (0, -1, -2):
-        return _("La conferenza è in fase di svolgimento!")
-    else:
-        return { 'HIGHLIGHT': None }
-        
-    if msg:
-        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
-    else:
-        msg = ''
-
-    attendee = assopy.attendeeCount()
-    if attendee > 0:
-        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
-        m2 = m2 % {'seat': 420 - attendee} 
-        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
-        msg += ' ' + m2
-    if msg:
-        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
-    return { 'HIGHLIGHT': msg }
+    return { 'HIGHLIGHT': '' }
+#    deadline = date(year = 2009, month = 5, day = 8)
+#    t = deadline - date.today()
+#
+#    if t.days > 1:
+#        msg = _("Mancano *%(days)d giorni* all'inizio della conferenza.")
+#        msg = msg % {'days': t.days}
+#    elif t.days == 1:
+#        msg = _("La conferenza inizia *domani*!")
+#    elif t.days in (0, -1, -2):
+#        return _("La conferenza è in fase di svolgimento!")
+#    else:
+#        return { 'HIGHLIGHT': None }
+#        
+#    if msg:
+#        msg = msg.replace('*', '<strong>', 1).replace('*', '</strong>')
+#    else:
+#        msg = ''
+#
+#    attendee = assopy.attendeeCount()
+#    if attendee > 0:
+#        m2 = _("Ci sono ancora *%(seat)d* posti disponibili")
+#        m2 = m2 % {'seat': 420 - attendee} 
+#        m2 = m2.replace('*', '<strong>', 1).replace('*', '</strong>')
+#        msg += ' ' + m2
+#    if msg:
+#        msg = '<a href="/pycon3/registrazione/">' + msg + '</a>'
+#    return { 'HIGHLIGHT': msg }
