ea472bd53ed2a643de9b7b2e8b1ed4b78e1623d1
dvd <dvd@develer.com>
2010-01-04 15:22:05 +0100

    rimossi i package conference e microblog, diventano dipendenze sterne

--- conference/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-"""
-Package contenente views/models e templatetags utili in più conferenze
-pycon-like. Idealmente in questo package trovano posto la gestione di modelli
-comuni (tipo le deadlines) templatetags di utilità (come la navigazione) e
-codice particolare come l'accesso ad assopy. Questo package *non* dovrebbe
-includere ne' template ne' file statici.
-"""
-
