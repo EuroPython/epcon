5d81d671ed452ecb0c19e33a176ff0f098cc8587
David Mugnai <dvd@develer.com>
2010-03-01 12:06:05 +0100

    se possibile innietto il DEFAULT_URL_PREFIX nel context dei template

--- conference/context_processors.py
+++ conference/context_processors.py
@@ -1,5 +1,7 @@
 import settings
 
+from django.conf import settings as dsettings
+
 def stuff(request):
     """
     Aggiunge le variabili relative alla STUFF directory
@@ -11,6 +13,7 @@ def stuff(request):
         'MEDIAPARTNER_LOGO_URL': '%smedia-partner/' % stuff,
         'SPEAKER_FACE_URL': '%sspeaker/' % stuff,
         'SLIDE_FILE_URL': '%sslides/' % stuff,
+        'DEFAULT_URL_PREFIX': getattr(dsettings, 'DEFAULT_URL_PREFIX', ''),
     }
     if settings.CONFERENCE_GOOGLE_MAPS:
         ctx['GOOGLE_MAPS_KEY'] = settings.CONFERENCE_GOOGLE_MAPS['key']
