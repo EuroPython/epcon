7650b6a8201676eb69c5fc0e985bc9d7038a55ad
dvd <dvd@gnx.it>
2011-01-26 01:33:41 +0100

    Manager per creare un'identità dal profilo che mi manda janrain

--- assopy/models.py
+++ assopy/models.py
@@ -1,17 +1,55 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
 
+from datetime import date
+
 class User(models.Model):
     user = models.OneToOneField("auth.User", related_name='assopy_user')
     # aggiungere id backend
 
+class UserIdentityManager(models.Manager):
+    def create_from_profile(self, user, profile):
+        """
+        crea una UserIdentity estraendo i dati dal profilo, risultato di una
+        chiamata ad auth_info; l'identity sarà associata all'utente (assopy)
+        passato.
+        """
+        identifier = UserIdentity(
+            identifier=profile['identifier'],
+            user=user,
+            provider=profile['providerName'],
+        )
+        try:
+            identifier.display_name = profile['name']['formatted']
+        except KeyError:
+            identifier.display_name = profile.get('displayName')
+        identifier.gender = profile.get('gender')
+
+        if 'birthday' in profile:
+            birthday = profile.get('birthday', '').split('-')
+            if birthday[0] == '0000':
+                birthday[0] = '1900'
+            identifier.birthday = date(*map(int, birthday))
+        try:
+            identifier.email = profile['verifiedEmail']
+        except KeyError:
+            identifier.email = profile.get('email')
+        identifier.url = profile.get('url')
+        identifier.photo = profile.get('photo')
+        identifier.phoneNumber = profile.get('phoneNumber')
+        try:
+            identifier.address = profile['address']['formatted']
+        except KeyError:
+            pass
+
+        identifier.save()
+        return identifier
+
 class UserIdentity(models.Model):
-    user = models.ForeignKey(User)
     identifier = models.CharField(max_length=255, primary_key=True)
+    user = models.ForeignKey(User)
     provider = models.CharField(max_length=255)
     display_name = models.TextField(blank=True)
-    first_name = models.CharField(max_length=255, blank=True)
-    last_name = models.CharField(max_length=255, blank=True)
     gender = models.CharField(max_length=10, blank=True)
     birthday = models.DateField(null=True)
     email = models.EmailField(blank=True)
@@ -19,3 +57,5 @@ class UserIdentity(models.Model):
     photo = models.URLField(verify_exists=False)
     phoneNumber = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
+
+    objects = UserIdentityManager()
