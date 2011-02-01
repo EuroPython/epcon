6e89f545e7e5284adbc971c84570b8b36cbe7891
dvd <dvd@develer.com>
2011-02-01 18:20:56 +0100

    esempio di creazione utente su assopy

--- assopy/clients/genro.py
+++ assopy/clients/genro.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from assopy import settings
+from assopy.clients.gnrbag import Bag
 
 from lxml import objectify
 
@@ -10,6 +11,22 @@ def _get(subpath):
     f = urllib.urlopen(url)
     return objectify.fromstring(f.read())
 
+def _post(subpath, bag=None):
+    url = settings.BACKEND + subpath
+    body = {}
+    if bag is not None:
+        body['data'] = bag.toXml()
+    f = urllib.urlopen(url, urllib.urlencode(body))
+    b = Bag()
+    b.fromXml(f.read())
+    return b
+
 def user(id):
     return _get('/users/%s' % id)   
 
+def create_user(first_name, last_name, email):
+    b = Bag()
+    b['firstname'] = first_name
+    b['lastname'] = last_name
+    b['email'] = email
+    return _post('/users/', b)
