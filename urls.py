be6a131cf1089d8e12253d4d341a16a57ca522be
dvd <dvd@gnx.it>
2011-02-06 16:40:30 +0100

    se sono in debug servo anche i media files

--- urls.py
+++ urls.py
@@ -31,6 +31,11 @@ if settings.DEBUG:
             'django.views.static.serve',
             {'document_root': os.path.join(path, k), 'show_indexes': True}
         ))
+    args.append((
+        r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+    ))
     from conference.settings import STUFF_URL, STUFF_DIR
     args.append((
         r'^%s(?P<path>.*)$' % STUFF_URL[1:],
