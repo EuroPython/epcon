47d6551e5b418254e60ea8bf379a5a06daaa34a7
David Mugnai <dvd@gnx.it>
2011-02-10 14:43:11 +0100

    aggiunto un .distinct per eliminare eventuali doppioni

--- conference/models.py
+++ conference/models.py
@@ -340,7 +340,7 @@ class Talk(models.Model):
             return None
 
     def get_all_speakers(self):
-        return self.speakers.all() | self.additional_speakers.all()
+        return (self.speakers.all() | self.additional_speakers.all()).distinct()
 
     def setAbstract(self, body, language=None):
         MultilingualContent.objects.setContent(self, 'abstracts', language, body)
