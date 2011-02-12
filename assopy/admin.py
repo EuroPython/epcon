57e50ba3fb939ad5d3dfd1b273f5b45be7e4e234
dvd <dvd@gnx.it>
2011-02-12 14:32:05 +0100

    AdminManager per ordine

--- assopy/admin.py
+++ assopy/admin.py
@@ -9,3 +9,19 @@ class CountryAdmin(admin.ModelAdmin):
     search_fields = ('name', 'printable_name', 'iso', 'numcode')
 
 admin.site.register(models.Country, CountryAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('code', '_user', '_created', '_items')
+
+    def _user(self, o):
+        return '%s %s' % (o.user.first_name, o.user.last_name)
+    _user.short_description = 'buyer'
+
+    def _items(self, o):
+        return sum( x[0] for x in o.orderitem_set.values_list('quantity'))
+    _items.short_description = '#Tickets'
+
+    def _created(self, o):
+        return o.created.strftime('%d %b %Y - %H:%M:%S')
+
+admin.site.register(models.Order, OrderAdmin)
