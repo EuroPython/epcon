fa495ac1a83b680e4941f4e7b303d355b8ea9188
dvd <dvd@gnx.it>
2011-02-12 15:17:40 +0100

    python experience come choices

--- p3/models.py
+++ p3/models.py
@@ -14,10 +14,18 @@ ATTENDEE_DIETS = (
     #('vegan', 'Vegan'),
     #('kosher', 'Kosher'),
 )
+ATTENDEE_EXPERIENCES = (
+    (0, '0'),
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+)
 class AttendeeProfile(models.Model):
     attendee = models.OneToOneField('conference.attendee', related_name='p3_conference')
     shirt_size = models.CharField(max_length=2, choices=ATTENDEE_SHIRT_SIZES)
-    python_experience = models.PositiveIntegerField(default=0)
+    python_experience = models.PositiveIntegerField(choices=ATTENDEE_EXPERIENCES, default=0)
     diet = models.CharField(max_length=10, choices=ATTENDEE_DIETS)
     tagline = models.CharField(max_length=60, blank=True, help_text='a (funny?) tagline for the attendee')
 
