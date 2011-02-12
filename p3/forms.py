e916115624a802bb4232ab33aa10bc0b105d1baa
dvd <dvd@gnx.it>
2011-02-12 15:18:09 +0100

    form per manipolare un AttendeeProfile

---
+++ p3/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+from django import forms
+import models
+
+class FormAttendee(forms.ModelForm):
+    attendee_id = forms.IntegerField(widget=forms.HiddenInput)
+    attendee_name = forms.CharField(max_length=60, required=False)
+
+    class Meta:
+        model = models.AttendeeProfile
+        exclude = ('attendee',)
