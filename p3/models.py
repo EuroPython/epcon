baed2cceebf9db01a89ed987d9a605686a105d14
dvd <dvd@gnx.it>
2011-02-12 09:34:53 +0100

    personalizzazione dell'attendee per p3

--- p3/models.py
+++ p3/models.py
@@ -1,1 +1,23 @@
 # -*- coding: UTF-8 -*-
+from django.db import models
+
+ATTENDEE_SHIRT_SIZES = (
+    ('s', 'S'),
+    ('m', 'M'),
+    ('l', 'L'),
+    ('xl', 'XL'),
+    ('xxl', 'XXL'),
+)
+ATTENDEE_DIETS = (
+    ('omnivorous', 'Vegetarian'),
+    ('vegetarian', 'Vegetarian'),
+    #('vegan', 'Vegan'),
+    #('kosher', 'Kosher'),
+)
+class AttendeeProfile(models.Model):
+    attendee = models.OneToOneField('conference.attendee', related_name='p3_conference')
+    shirt_size = models.CharField(max_length=2, choices=ATTENDEE_SHIRT_SIZES)
+    python_experience = models.PositiveIntegerField(default=0)
+    diet = models.CharField(max_length=10, choices=ATTENDEE_DIETS)
+    tagline = models.CharField(max_length=60, blank=True, help_text='a (funny?) tagline for the attendee')
+
