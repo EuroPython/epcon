38debec60e7368aba34b1abf855469d7ab805a43
dvd <dvd@gnx.it>
2011-02-12 16:03:52 +0100

    default per shirt_size e diet (fanno comodo nella UI)

--- p3/models.py
+++ p3/models.py
@@ -24,8 +24,8 @@ ATTENDEE_EXPERIENCES = (
 )
 class AttendeeProfile(models.Model):
     attendee = models.OneToOneField('conference.attendee', related_name='p3_conference')
-    shirt_size = models.CharField(max_length=2, choices=ATTENDEE_SHIRT_SIZES)
+    shirt_size = models.CharField(max_length=2, choices=ATTENDEE_SHIRT_SIZES, default='l')
     python_experience = models.PositiveIntegerField(choices=ATTENDEE_EXPERIENCES, default=0)
-    diet = models.CharField(max_length=10, choices=ATTENDEE_DIETS)
+    diet = models.CharField(max_length=10, choices=ATTENDEE_DIETS, default='omnivorous')
     tagline = models.CharField(max_length=60, blank=True, help_text='a (funny?) tagline for the attendee')
 
