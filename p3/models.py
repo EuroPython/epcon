603df7461cb710a71aaa2bb6e4e176f1ac5102f7
dvd <dvd@gnx.it>
2011-02-13 13:40:39 +0100

    rinominato il modello attendeeprofile in ticketconference

--- p3/models.py
+++ p3/models.py
@@ -22,8 +22,8 @@ ATTENDEE_EXPERIENCES = (
     (4, '4'),
     (5, '5'),
 )
-class AttendeeProfile(models.Model):
-    attendee = models.OneToOneField('conference.attendee', related_name='p3_conference')
+class TicketConference(models.Model):
+    attendee = models.OneToOneField('conference.ticket', related_name='p3_conference')
     shirt_size = models.CharField(max_length=2, choices=ATTENDEE_SHIRT_SIZES, default='l')
     python_experience = models.PositiveIntegerField(choices=ATTENDEE_EXPERIENCES, default=0)
     diet = models.CharField(max_length=10, choices=ATTENDEE_DIETS, default='omnivorous')
