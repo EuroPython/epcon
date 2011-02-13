b027790bb5f7ef5885937e1ef32df73506ed95e7
dvd <dvd@gnx.it>
2011-02-13 14:32:10 +0100

    unique e blank non vanno bene d'accordo

--- assopy/models.py
+++ assopy/models.py
@@ -134,7 +134,7 @@ class UserManager(models.Manager):
 class User(models.Model):
     user = models.OneToOneField("auth.User", related_name='assopy_user')
     token = models.CharField(max_length=36, unique=True, null=True)
-    assopy_id = models.CharField(max_length=22, blank=True, unique=True)
+    assopy_id = models.CharField(max_length=22, null=True, unique=True)
     verified = models.BooleanField(default=False)
     photo = models.ImageField(null=True, upload_to='assopy/users')
     twitter = models.CharField(max_length=20, blank=True)
