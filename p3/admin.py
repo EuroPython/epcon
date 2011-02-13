f4a7eff47e909bc856992d9f937ece7472761bb8
dvd <dvd@gnx.it>
2011-02-13 09:22:59 +0100

    Custom model admin per l'attendee di p3 (non molto utile per il momento)

--- p3/admin.py
+++ p3/admin.py
@@ -1,1 +1,17 @@
 # -*- coding: UTF-8 -*-
+from django.contrib import admin
+from conference.admin import AttendeeAdmin
+from conference.models import Attendee
+from p3.models import AttendeeProfile
+
+class AttendeeProfileAdmin(AttendeeAdmin):
+    list_display = AttendeeAdmin.list_display + ('_assigned',)
+    
+    def _assigned(self, o):
+        try:
+            return o.p3_conference.assigned_to
+        except AttendeeProfile.DoesNotExist:
+            return ''
+
+admin.site.unregister(Attendee)
+admin.site.register(Attendee, AttendeeProfileAdmin)
