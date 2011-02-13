64995e071363d4e531090642e0aebdc4255ce3af
dvd <dvd@gnx.it>
2011-02-13 15:23:20 +0100

    assegnazione ticket, invio email

--- p3/views.py
+++ p3/views.py
@@ -3,12 +3,14 @@ from django import http
 from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.template.loader import render_to_string
 
-from assopy.models import UserIdentity
+from assopy.models import User, UserIdentity
 from assopy.views import render_to, HttpResponseRedirectSeeOther
 
 import forms
@@ -16,6 +18,7 @@ import models
 from conference.models import Ticket
 
 import logging
+import uuid
 
 log = logging.getLogger('p3.views')
 
@@ -31,9 +34,9 @@ def tickets(request):
         'tickets': tickets,
     }
 
-def _assign_ticket(attendee, email):
+def _assign_ticket(ticket, email):
     try:
-        recipient = auth.models.User.objects(email=email)
+        recipient = auth.models.User.objects.get(email=email)
     except auth.models.User.DoesNotExist:
         try:
             # qui uso filter + [0] invece che .get perchè potrebbe accadere
@@ -44,18 +47,29 @@ def _assign_ticket(attendee, email):
             # (nota che il backend di autenticazione già verifica che la stessa
             # email non venga usata due volte per creare utenti django) perché
             # in ogni caso si tratta di email verificate da servizi esterni.
-            recipient = UserIdentity.objects.filter(email=email)[0]
+            recipient = UserIdentity.objects.filter(email=email)[0].user.user
         except IndexError:
             recipient = None
     if recipient is None:
         log.info('No user found for the email "%s"; time to create a new one', email)
+        u = User.objects.create_user(email=email, token=True, send_mail=False)
+        recipient = u.user
+        name = email
     else:
-        log.info('Found a local user (%s) for the email "%s"', user, email)
+        log.info('Found a local user (%s) for the email "%s"', recipient, email)
+        if not recipient.assopy_user.token:
+            recipient.assopy_user.token = str(uuid.uuid4())
+            recipient.assopy_user.save()
+        name = recipient.assopy_user.name()
     ctx = {
-        'recipient': recipient,
-        'attendee': attendee,
+        'name': name,
+        'ticket': ticket,
         'conference': 'Europython 2011',
+        'link': settings.DEFAULT_URL_PREFIX + reverse('p3-user', kwargs={'token': recipient.assopy_user.token}),
     }
+    body = render_to_string('p3/emails/ticket_assigned.txt', ctx)
+    send_mail('Ticket assigned to you', body, 'info@pycon.it', [email])
+
 @login_required
 @transaction.commit_on_success
 def ticket(request, tid):
@@ -79,6 +93,7 @@ def ticket(request, tid):
         if assigned_to != x.assigned_to:
             if x.assigned_to:
                 log.info('ticket assigned to "%s"', x.assigned_to)
+                _assign_ticket(t, x.assigned_to)
             else:
                 log.info('ticket reclaimed (previously assigned to "%s")', assigned_to)
         x.save()
