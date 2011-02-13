ef6272f6a792bbc9fd61c56dd9762aa4d535d3ea
dvd <dvd@gnx.it>
2011-02-13 22:24:46 +0100

    .trim per le stringhe

--- p3/static/p5/j/utils.js
+++ p3/static/p5/j/utils.js
@@ -29,3 +29,12 @@ function feedback(msg) {
         setTimeout(function() { f.remove() }, 10000);
     }
 }
+
+// http://stackoverflow.com/questions/1418050/string-strip-for-javascript
+if(typeof(String.prototype.trim) === "undefined")
+{
+    String.prototype.trim = function() 
+    {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
