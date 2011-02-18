12b5ea049d79753a693ca57e2d102862033303af
David Mugnai <dvd@develer.com>
2011-02-18 17:36:59 +0100

    typo

--- conference/forms.py
+++ conference/forms.py
@@ -22,7 +22,7 @@ class SubmissionForm(forms.Form):
     title = forms.CharField(label=_('Talk title'), max_length=100, widget=forms.TextInput(attrs={'size': 40}))
     training = forms.BooleanField(
         label=_('Training'),
-        help_text=_('Check if you are willing to also deliver a 4-hours hands-on training on this subject.<br />See te Call for paper for details.'),
+        help_text=_('Check if you are willing to also deliver a 4-hours hands-on training on this subject.<br />See the Call for paper for details.'),
         required=False,)
     duration = forms.TypedChoiceField(
         label=_('Suggested duration'),
@@ -99,7 +99,7 @@ class TalkForm(forms.Form):
     title = forms.CharField(label=_('Talk title'), max_length=100, widget=forms.TextInput(attrs={'size': 40}))
     training = forms.BooleanField(
         label=_('Training'),
-        help_text=_('Check if you are willing to also deliver a 4-hours hands-on training on this subject.<br />See te Call for paper for details.'),
+        help_text=_('Check if you are willing to also deliver a 4-hours hands-on training on this subject.<br />See the Call for paper for details.'),
         required=False,)
     duration = forms.TypedChoiceField(
         label=_('Suggested duration'),
