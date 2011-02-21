a40a0beb7c203168e741bab1c027265f277f6d3c
dvd <dvd@develer.com>
2011-02-21 16:32:27 +0100

    rimosse url inutili
    Former-commit-id: cfe2eeb641943ad55e49be30c725be91596ffe49

--- urls.py
+++ urls.py
@@ -7,19 +7,17 @@ admin.autodiscover()
 from p3.forms import P3SubmissionForm, P3SubmissionAdditionalForm, P3TalkForm
 
 urlpatterns = patterns('',
+    (r'^accounts/', include('assopy.urls')),
     (r'^admin/templatesadmin/', include('templatesadmin.urls')),
     (r'^admin/', include(admin.site.urls)),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
-    (r'^assopy/$', 'conference.views.genro_wrapper'),
-    (r'^p3/', include('p3.urls')),
-    (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^blog/', include('microblog.urls')),
     url(r'^conference/paper-submission/$', 'conference.views.paper_submission', {'submission_form': P3SubmissionForm, 'submission_additional_form': P3SubmissionAdditionalForm, }, name='conference-paper-submission'),
     url(r'^conference/talks/(?P<slug>.*)', 'conference.views.talk', {'talk_form': P3TalkForm}, name='conference-talk'),
     (r'^conference/', include('conference.urls')),
     (r'^comments/', include('django.contrib.comments.urls')),
     (r'^hcomments/', include('hcomments.urls')),
-    (r'^accounts/', include('assopy.urls')),
+    (r'^p3/', include('p3.urls')),
+    (r'^i18n/', include('django.conf.urls.i18n')),
 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
