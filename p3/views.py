e90954aa70ef134dd74eb0a03fd38aac1dc680f3
dvd <dvd@gnx.it>
2011-03-03 23:25:36 +0100

    aggiorno l'html dopo la modifica del biglietto
    Former-commit-id: e28984b961f603b88ed32e0683f88fa605f21744

--- p3/views.py
+++ p3/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect, render_to_response
-from django.template import RequestContext
+from django.template import RequestContext, Template
 from django.template.loader import render_to_string
 
 from assopy.forms import BillingData
@@ -138,9 +138,10 @@ def ticket(request, tid):
             form.save()
             
     if request.is_ajax():
-        # piccolo aiuto per le chiamate ajax che non sono interessate alla
-        # risposta
-        return http.HttpResponse('')
+        # restituisco il rendering del nuovo biglietto, così il chiamante può
+        # mostrarlo facilmente
+        tpl = Template('{% load p3 %}{% render_ticket t %}')
+        return http.HttpResponse(tpl.render(RequestContext(request, {'t': t})))
     else:
         return HttpResponseRedirectSeeOther(reverse('p3-tickets'))
 
