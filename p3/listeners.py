3d66edc1c3eac3368faa74e989f6d355f411731e
dvd <dvd@develer.com>
2011-03-08 17:54:59 +0100

    listener per informare assopy dei biglietti assegnati ad un utente
    Former-commit-id: d210609dfdee6edccb9e074baf3b87f68e951e94

--- p3/listeners.py
+++ p3/listeners.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
+import models
+
+from django.conf import settings
 from email_template import utils
-from assopy.models import order_created, purchase_completed
+from assopy.models import order_created, purchase_completed, ticket_for_user
 
 def on_order_created(sender, **kwargs):
     if sender.method == 'bank':
@@ -23,3 +26,9 @@ def on_purchase_completed(sender, **kwargs):
     ).send()
 
 purchase_completed.connect(on_purchase_completed)
+
+def on_ticket_for_user(sender, **kwargs):
+    tickets = models.TicketConference.objects.available(sender.user, settings.CONFERENCE_CONFERENCE)
+    map(kwargs['tickets'].append, tickets)
+
+ticket_for_user.connect(on_ticket_for_user)
