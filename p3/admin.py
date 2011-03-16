da93eff5faf3dbc1af1fc6152a7d6c0ff8471592
dvd <dvd@develer.com>
2011-03-16 14:17:07 +0100

    aggiunto supporto donazioni
    Former-commit-id: 34d96c671fdff274bdfeef54238fd0c33f2a71ab

--- p3/admin.py
+++ p3/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from conference.admin import TicketAdmin
 from conference.models import Ticket
-from p3.models import TicketConference
+from p3 import models
 
 class TicketConferenceAdmin(TicketAdmin):
     list_display = TicketAdmin.list_display + ('_order', '_assigned',)
@@ -13,8 +13,21 @@ class TicketConferenceAdmin(TicketAdmin):
     def _assigned(self, o):
         try:
             return o.p3_conference.assigned_to
-        except TicketConference.DoesNotExist:
+        except models.TicketConference.DoesNotExist:
             return ''
 
 admin.site.unregister(Ticket)
 admin.site.register(Ticket, TicketConferenceAdmin)
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('_name', 'date', 'amount')
+    list_select_related = True
+    search_fields = ('user__user__first_name', 'user__user__last_name', 'user__user__email')
+    date_hierarchy = 'date'
+
+    def _name(self, o):
+        return o.user.name()
+    _name.short_description = 'name'
+    _name.admin_order_field = 'user__user__first_name'
+
+admin.site.register(models.Donation, DonationAdmin)
