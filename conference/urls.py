f43efa31c3778d7002330904d10c78a857f3ca09
David Mugnai <dvd@gnx.it>
2011-04-10 00:29:30 +0200

    view xml di spaker e talk

--- conference/urls.py
+++ conference/urls.py
@@ -2,11 +2,13 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^speakers/(?P<slug>.*)', 'conference.views.speaker', name = 'conference-speaker'),
+    url(r'^speakers/(?P<slug>[\w-]+).xml', 'conference.views.speaker_xml', name = 'conference-speaker-xml'),
+    url(r'^speakers/(?P<slug>[\w-]+)', 'conference.views.speaker', name = 'conference-speaker'),
     url(r'^speaker/admin/image_upload', 'conference.views.speaker_admin_image_upload', name = 'conference-speaker-img-upload'),
     url(r'^talks/admin/upload', 'conference.views.talk_admin_upload', name = 'conference-talks-upload'),
     url(r'^talks/report', 'conference.views.talk_report', name = 'conference-talk-report'),
-    url(r'^talks/(?P<slug>.*)', 'conference.views.talk', name = 'conference-talk'),
+    url(r'^talks/(?P<slug>[\w-]+).xml$', 'conference.views.talk_xml', name = 'conference-talk-xml'),
+    url(r'^talks/(?P<slug>[\w-]+)$', 'conference.views.talk', name = 'conference-talk'),
     url(r'^schedule/(?P<conference>.*)/(?P<slug>[^\.]*).xml/speakers.xml$', 'conference.views.schedule_speakers_xml', name = 'conference-schedule-speakers-xml'),
     url(r'^schedule/(?P<conference>.*)/(?P<slug>[^\.]*).xml/?$', 'conference.views.schedule_xml', name = 'conference-schedule-xml'),
     url(r'^schedule/(?P<conference>.*)/(?P<slug>[^\.]*)$', 'conference.views.schedule', name = 'conference-schedule'),
