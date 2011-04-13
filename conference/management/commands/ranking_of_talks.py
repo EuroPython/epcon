695170a9bfff62d7eb1a6f93a086c8afd289099e
David Mugnai <dvd@develer.com>
2011-04-13 18:42:58 +0200

    management command per calcolare la classifca dei talk di una conferenza

---
+++ conference/management/commands/ranking_of_talks.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from conference import models
+from conference import utils
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            conference = args[0]
+        except IndexError:
+            raise CommandError('conference not specified')
+
+        talks = models.Talk.objects.filter(conference=conference)
+        votes = models.VotoTalk.objects.filter(talk__in=talks)
+        users = set(x.user_id for x in votes)
+        print '%d talks / %d users / %d votes' % (talks.count(), len(users), len(votes))
+        for ix, t in enumerate(utils.ranking_of_talks(talks)):
+            print ix+1, '-', t.id, '-', t.title
