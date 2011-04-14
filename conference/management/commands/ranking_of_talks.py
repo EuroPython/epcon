e6dbb1a4e55d46139d91eed7dae37b14611a8f12
David Mugnai <dvd@develer.com>
2011-04-14 09:52:29 +0200

    aggiunto il nome dello speaker nel management command

--- conference/management/commands/ranking_of_talks.py
+++ conference/management/commands/ranking_of_talks.py
@@ -15,4 +15,4 @@ class Command(BaseCommand):
         users = set(x.user_id for x in votes)
         print '%d talks / %d users / %d votes' % (talks.count(), len(users), len(votes))
         for ix, t in enumerate(utils.ranking_of_talks(talks)):
-            print ix+1, '-', t.id, '-', t.title
+            print ix+1, '-', t.id, '-', t.title.encode('utf-8'), '(%s)' % (', '.join(s.name.encode('utf-8') for s in t.get_all_speakers()),)
