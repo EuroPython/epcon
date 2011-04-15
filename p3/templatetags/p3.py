fa6605f139d44debc771cf1096f78daf9d38d594
dvd <dvd@develer.com>
2011-04-15 15:53:53 +0200

    utilizzo il nuovo latest_tweets in conference
    Former-commit-id: f9143dcac42e1b51d2f863ce08c51861e5f30d1e

--- p3/templatetags/p3.py
+++ p3/templatetags/p3.py
@@ -15,11 +15,12 @@ from django.core.cache import cache
 from django.template import Context
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
-from fancy_tag import fancy_tag
-import twitter
 
 from conference import models as ConferenceModels
 from conference.settings import STUFF_DIR, STUFF_URL
+
+import twitter
+from fancy_tag import fancy_tag
 
 mimetypes.init()
 
@@ -107,100 +108,17 @@ def box_talks_conference(context, talks):
 
 @register.inclusion_tag('p3/box_latest_tweets.html', takes_context=True)
 def box_latest_tweets(context):
-    return context
-
-# I tweet venogno scaricati ogni 5 minuti, a volte però twitter non risponde o
-# solleva un errore, in questo caso mostriamo gli ultimi tweet validi che
-# abbiamo.
-_LastGoodTweets = []
-
-@register.tag
-def latest_tweets(parser, token):
-    """
-    {% latest_tweets [ n_tweets ] as var %}
-    inserisce in var la lista degli ultimi tweet (opzionalmente è possibile
-    selezionare il numero di tweet da visualizzare).
-    """
-    def getLatestTweets(n_tweets):
-        global _LastGoodTweets
-        key = 'bs_latest_tweets'
-        tweets = cache.get(key)
-        if not tweets:
-            api = twitter.Api()
-            # il modulo twitter fornisce un meccanismo di caching, ma
-            # preferisco disabilitarlo per due motivi:
-            #
-            # 1. voglio usare la cache di django, in questo modo modificando i
-            # settings modifico anche la cache per twitter
-            # 2. di default, ma è modificabile con le api pubbliche, la cache
-            # del modulo twitter è file-based e sbaglia a decidere la directory
-            # in cui salvare i file; finisce per scrivere in
-            # /tmp/python.cache_nobody/... che non è detto sia scrivibile
-            # (sopratutto se la directory è stata creata da un altro processo
-            # che gira con un utente diverso).
-            api.SetCache(None)
-            try:
-                tweets = api.GetUserTimeline(settings.P3_TWITTER_USER, count=n_tweets)
-            except (ValueError, urllib2.HTTPError):
-                # ValueError: a volte twitter.com non risponde correttamente, e
-                # twitter (il modulo) non verifica. Di conseguenza viene
-                # sollevato un ValueError quando twitter (il modulo) tenta
-                # parsare un None come se fosse una stringa json.
-
-                # HTTPError: a volte twitter.com non risponde proprio :) (in
-                # realtà risponde con un 503)
-
-                # Spesso questi errori durano molto poco, ma per essere gentile
-                # con twitter cacho un risultato nullo per poco tempo.
-                tweets = None
-            except:
-                # vista la stabilità di twitter meglio cachare tutto per
-                # evitare errori sul nostro sito
-                tweets = None
-            if not tweets:
-                tweets = _LastGoodTweets
-            else:
-                _LastGoodTweets = tweets
-                cache.set(key, tweets, 60 * 5)
-        return [{
-                "text": tweet.GetText(),
-                "timestamp": tweet.GetCreatedAtInSeconds(),
-                "followers_count": tweet.user.GetFollowersCount(),
-                "id": tweet.GetId(),
-            } for tweet in tweets]
-
-    contents = token.split_contents()
-    tag_name = contents[0]
-    if contents[-2] != 'as':
-        raise template.TemplateSyntaxError("%r tag had invalid arguments" %tag_name)
-    var_name = contents[-1]
-    if len(contents) > 3:
-        n_tweets = contents[1]
-    else:
-        n_tweets = 3
-
-    class TweetNode(template.Node):
-        def __init__(self, n_tweets, var_name):
-            self.var_name = var_name
-            self.n_tweets = n_tweets
-        def render(self, context):
-            context[self.var_name] = getLatestTweets(self.n_tweets)
-            return ''
-    return TweetNode(n_tweets, var_name)    
+    ctx = Context(context)
+    ctx.update({
+        'screen_name': settings.P3_TWITTER_USER,
+    })
+    return ctx
 
 @register.filter
 def render_time(tweet, args=None):
     time = tweet["timestamp"]
     time = datetime.datetime.fromtimestamp(time)
     return time.strftime("%d-%m-%y @ %H:%M") 
-
-@register.filter
-def convert_links(tweet, args=None):
-    text = tweet["text"]
-    text = re.sub(r'(https?://[^\s]*)', r'<a href="\1">\1</a>', text)
-    text = re.sub(r'@([^\s]*)', r'@<a href="http://twitter.com/\1">\1</a>', text)
-    text = re.sub(r'([^&])#([^\s]*)', r'\1<a href="http://twitter.com/search?q=%23\2">#\2</a>', text)
-    return mark_safe(text)
 
 @register.filter
 def check_map(page):
