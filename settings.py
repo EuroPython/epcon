b3046832d3b2472528fbcc5c076313ed2f17a4d7
dvd <dvd@develer.com>
2011-05-30 16:27:57 +0200

    template  loader, default 1.3
    Former-commit-id: 14dcda3dacc5c2b90e0e3f0ee5fa47bf0bcc19cf

--- settings.py
+++ settings.py
@@ -67,9 +67,8 @@ SECRET_KEY = 'kc-yf%25(%6e870u&z$)!rq@dm(b2giam-p$q$!vg#dv+j0!57'
 
 # List of callables that know how to import templates from various sources.
 #TEMPLATE_LOADERS = (
-#    'django.template.loaders.filesystem.load_template_source',
-#    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+#    'django.template.loaders.filesystem.Loader',
+#    'django.template.loaders.app_directories.Loader',
 #)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
