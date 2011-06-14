3d2789fd69778e574d58f67e8593b9d17823906c
dvd <dvd@develer.com>
2011-06-14 17:13:31 +0200

    inizio integrazione stats nel package conference
    Former-commit-id: c4078c91624724e56df7330e4a76819a95ede393

---
+++ p3/utils.py
@@ -0,0 +1,105 @@
+# -*- coding: UTF-8 -*-
+from conference.models import Ticket
+from p3 import models
+from django.db.models import Count, Q
+from collections import defaultdict
+
+def conference_stats(conference, stat=None):
+    stats = []
+    tickets = Ticket.objects.filter(
+        orderitem__order___complete=True,
+        fare__ticket_type='conference',
+        fare__conference=conference,
+    ).select_related('p3_conference', 'orderitem__order__user__user')
+    compiled = tickets.exclude(p3_conference=None).exclude(name='')
+    not_compiled = tickets.exclude(p3_conference=None).filter(name='') | tickets.filter(p3_conference=None)
+    if stat in (None, 'all'):
+        stats.append({
+            'code': 'all',
+            'title': 'Biglietti venduti',
+            'count': tickets.count(),
+            'have_details': True,
+            'details': tickets,
+        })
+    if stat in (None, 'not_compiled'):
+        stats.append({
+            'code': 'not_compiled',
+            'title': 'Biglietti non compilati',
+            'count': not_compiled.count(),
+            'have_details': True,
+            'details': not_compiled,
+        })
+    if stat in (None, 'compiled'):
+        stats.append({
+            'code': 'compiled',
+            'title': 'Biglietti compilati',
+            'count': compiled.count(),
+            'have_details': True,
+            'details': compiled,
+        })
+    if stat is None or stat.startswith('tshirt_'):
+        sizes = dict(models.TICKET_CONFERENCE_SHIRT_SIZES)
+        for x in compiled.values('p3_conference__shirt_size').annotate(c=Count('id')):
+            scode = 'tshirt_%s' % x['p3_conference__shirt_size']
+            if stat in (None, scode):
+                stats.append({
+                    'code': scode,
+                    'title': 'Taglia maglietta: %s' % sizes.get(x['p3_conference__shirt_size']),
+                    'count': x['c'], 
+                    'have_details': True,
+                    'details': compiled.filter(p3_conference__shirt_size=x['p3_conference__shirt_size']),
+                })
+    if stat is None or stat.startswith('diet_'):
+        diets = dict(models.TICKET_CONFERENCE_DIETS)
+        for x in compiled.values('p3_conference__diet').annotate(c=Count('id')):
+            scode = 'diet_%s' % x['p3_conference__diet']
+            if stat in (None, scode):
+                stats.append({
+                    'code': scode,
+                    'title': 'Dieta: %s' % diets.get(x['p3_conference__diet']),
+                    'count': x['c'], 
+                    'have_details': True,
+                    'details': compiled.filter(p3_conference__diet=x['p3_conference__diet']),
+                })
+    if stat is None or stat.startswith('days_'):
+        days = defaultdict(lambda: 0)
+        for x in compiled:
+            data = filter(None, map(lambda x: x.strip(), x.p3_conference.days.split(',')))
+            if not data:
+                days['x'] += 1
+            else:
+                for v in data:
+                    days[v] += 1
+        compiled_count = compiled.count()
+        not_compiled_count = not_compiled.count()
+        for day, count in days.items():
+            scode = 'days_%s' % day
+            if stat in (None, scode):
+                if day != 'x':
+                    info = float(count) / (compiled_count - days['x']) * (compiled_count + not_compiled_count)
+                else:
+                    info = 0.0
+                stats.append({
+                    'code': scode,
+                    'title': 'Giorno di presenza: %s' % day,
+                    'count': count,
+                    'have_details': False,
+                    'additional_info': info,
+                })
+
+    if stat in (None, 'sim_not_compiled'):
+        tickets = Ticket.objects.filter(
+            orderitem__order___complete=True,
+            fare__code__startswith='SIM',
+            fare__conference=conference,
+        ).select_related('p3_conference_sim', 'orderitem__order__user__user')
+        qs = tickets.exclude(p3_conference_sim=None).filter(Q(name='')|Q(p3_conference_sim__document='')) | tickets.filter(p3_conference_sim=None)
+        stats.append({
+            'code': 'sim_not_compiled',
+            'title': 'Biglietti SIM non completi',
+            'count': qs.count(),
+            'have_details': True,
+            'details': qs,
+        })
+    return stats
+
