51d963fd472815956936d2bb8a3b3561b7169ff5
dvd <dvd@develer.com>
2011-06-16 18:25:58 +0200

    jokes
    Former-commit-id: 6995cee4ec7abd5a256bca44349b77a9513f0bc5

--- other_stuff/badge/ep2011/conf.py
+++ other_stuff/badge/ep2011/conf.py
@@ -1,4 +1,6 @@
+# -*- coding: UTF-8 -*-
 import Image, ImageFont
+import os.path
 
 _FONT_NAME = 'Arial_Unicode.ttf'
 _FONTS = {
@@ -6,19 +8,13 @@ _FONTS = {
     'name_small': ImageFont.truetype(_FONT_NAME, 10 * 8),
     'info': ImageFont.truetype(_FONT_NAME, 8 * 8),
 }
+images = 'allyourbase.png apple.png bertos.png bofh.png bycicle.png cake.png camcorder.png campi.png challenge.png ciuccio.png disqus.png dontpanic.png enthought.png fedora.png fluidinfo.png f-secure.png giullare.png iphone.png lundh.png male.png maya.png merengue.png moinmoin.png openquake.png openstack.png panino.png perl.png plaster.png postgres.png pycharm.png pyhp.png pypy.png pyside.png rapple.png riverbank.png royale.png skeleton.png sourceforge.png spiderman.png spotify.png stackless.png taz.png ubuntu.png wizard.png'
 _ICONS = {
     None: Image.open('logo.png').convert('RGBA').resize((64, 64)),
-    'apple': Image.open('apple.png').convert('RGBA').resize((64, 64)),
-    'cake': Image.open('cake.png').convert('RGBA').resize((64, 64)),
-    'camcorder': Image.open('camcorder.png').convert('RGBA').resize((64, 64)),
-    'campi': Image.open('campi.png').convert('RGBA').resize((64, 64)),
-    'challenge': Image.open('challenge.png').convert('RGBA').resize((64, 64)),
-    'ciuccio': Image.open('ciuccio.png').convert('RGBA').resize((64, 64)),
-    'plaster': Image.open('plaster.png').convert('RGBA').resize((64, 64)),
-    'postgres': Image.open('postgres.png').convert('RGBA').resize((64, 64)),
-    'skeleton': Image.open('skeleton.png').convert('RGBA').resize((64, 64)),
-    'wizard': Image.open('wizard.png').convert('RGBA').resize((64, 64)),
 }
+
+for _ in images.split(' '):
+    _ICONS[os.path.splitext(_)[0]] = Image.open('jokes/' + _).convert('RGBA').resize((64, 64))
 
 WASTE = 3
 
@@ -86,11 +82,21 @@ def tickets(tickets):
 
 def ticket(image, ticket):
     import string
-    try:
-        first_name, last_name = ticket['name'].split(' ', 1)
-    except ValueError:
-        first_name = ticket['name']
-        last_name = ''
+    _multi = (
+        'pier maria', 'gian battista', 'arnaldo miguel', 'dr. yves', 'dr. stefan', 'mr yun',
+    )
+    check = ticket['name'].lower()
+    for x in _multi:
+        if check.startswith(x):
+            first_name = ticket['name'][:len(x)].strip()
+            last_name = ticket['name'][len(x):].strip()
+            break
+    else:
+        try:
+            first_name, last_name = ticket['name'].split(' ', 1)
+        except ValueError:
+            first_name = ticket['name']
+            last_name = ''
     tagline = ticket.get('tagline', '').strip()
 
     first_name = first_name.upper().strip()
@@ -111,13 +117,13 @@ def ticket(image, ticket):
     lname = ticket['name'].lower()
     ltag = tagline.lower()
     if lname == 'lorenzo berni':
-        logo = _ICONS['apple']
+        logo = _ICONS['rapple']
     elif lname == 'paolo romolini':
         logo = _ICONS['campi']
     elif lname == 'francesco pallanti':
         logo = _ICONS['cake']
     elif lname == 'lorenzo mancini':
-        logo = _ICONS['wizard']
+        logo = _ICONS['male']
     elif lname == 'giovanni bajo':
         logo = _ICONS['skeleton']
     elif lname == 'dario trovato':
@@ -125,6 +131,72 @@ def ticket(image, ticket):
         ticket['experience'] = 5
     elif lname == 'simone federici':
         logo = _ICONS['plaster']
+    elif lname in ('didrik pinte', 'damian campbell'):
+        logo = _ICONS['enthought']
+    elif lname in ('matti airas', 'thomas perl'):
+        logo = _ICONS['pyside']
+    elif lname == 'caroline tice':
+        logo = _ICONS['apple']
+    elif lname == 'phil thompson':
+        logo = _ICONS['riverbank']
+    elif lname in ('armin rigo', 'antonio cuni', 'laura creighton', 'jacob hallén'):
+        logo = _ICONS['pypy']
+    elif lname == 'valentino volonghi':
+        logo = _ICONS['giullare']
+    elif lname == 'christian tismer':
+        logo = _ICONS['stackless']
+    elif lname in ('fabio rotondo', 'andrea rotondo', 'giulio porta', 'davide davin'):
+        logo = _ICONS['pyhp']
+    elif lname in ('pär von zweigbergk', 'jon åslund', 'nick barkas'):
+        logo = _ICONS['spotify']
+    elif lname == 'fredrik lundh':
+        logo = _ICONS['lundh']
+    elif lname == 'taz':
+        logo = _ICONS['taz']
+    elif lname in ('santtu jarvi', 'hannu maaranen', 'mathias fröjdman'):
+        logo = _ICONS['f-secure']
+    elif lname == 'nicola \'woody\' ferruzzi':
+        logo = _ICONS['iphone']
+    elif lname in ('nicola larosa', 'paolo sammicheli', 'andrea corbellini'):
+        logo = _ICONS['ubuntu']
+    elif lname == 'david cramer':
+        logo = _ICONS['disqus']
+    elif lname == 'reimar bauer':
+        logo = _ICONS['moinmoin']
+    elif lname == 'nikola kudus':
+        logo = _ICONS['maya']
+    elif lname == 'gianluca sforna':
+        logo = _ICONS['fedora']
+    elif lname == 'tomaž šolc':
+        logo = _ICONS['perl']
+    elif lname == 'sten spans':
+        logo = _ICONS['dontpanic']
+    elif lname == 'gianni moschini':
+        logo = _ICONS['allyourbase']
+    elif lname in ('andrea cerisara', 'marco milanesi', 'lars butler', 'john tarter'):
+        logo = _ICONS['openquake']
+    elif lname in ('esteve fernandez', 'nicholas tollervey'):
+        logo = _ICONS['fluidinfo']
+    elif lname == 'manuel saelices':
+        logo = _ICONS['merengue']
+    elif lname == 'harry percival':
+        logo = _ICONS['panino']
+    elif lname == 'morten brekkevold':
+        logo = _ICONS['bycicle']
+    elif lname == 'andreas dreier':
+        logo = _ICONS['royale']
+    elif lname == 'robin harrison':
+        logo = _ICONS['spiderman']
+    elif lname == 'thierry carrez':
+        logo = _ICONS['openstack']
+    elif lname == 'mark ramm':
+        logo = _ICONS['sourceforge']
+    elif lname == 'francesco sacchi':
+        logo = _ICONS['bertos']
+    elif lname == 'andrey vlasovskikh':
+        logo = _ICONS['pycharm']
+    elif lname in ('stefano fedrigo', 'simone roselli'):
+        logo = _ICONS['bofh']
     elif 'drop table' in ltag:
         logo = _ICONS['ciuccio']
     elif 'challenge accepted' in ltag:
