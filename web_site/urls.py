1c4c4660203e937297ae850b101e2aba6043a808
David Mugnai <dvd@develer.com>
2012-02-10 10:01:57 +0100

    eliminato codice commentato
    Former-commit-id: cb6374b9b89c7f7eced929e54704c53badcd559f

--- web_site/urls.py
+++ web_site/urls.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import *
-from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -19,36 +18,18 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^blog/', include('microblog.urls')),
     (r'^comments/', include('django.contrib.comments.urls')),
-    url(r'^conference/paper-submission/$', 'conference.views.paper_submission', {'submission_form': P3SubmissionForm, 'submission_additional_form': P3SubmissionAdditionalForm, }, name='conference-paper-submission'),
-    url(r'^conference/talks/(?P<slug>[\w-]+)$', 'conference.views.talk', {'talk_form': P3TalkForm}, name='conference-talk'),
+    url(r'^conference/paper-submission/$', 'conference.views.paper_submission',
+        {'submission_form': P3SubmissionForm, 'submission_additional_form': P3SubmissionAdditionalForm, },
+        name='conference-paper-submission'),
+    url(r'^conference/talks/(?P<slug>[\w-]+)$', 'conference.views.talk',
+        {'talk_form': P3TalkForm},
+        name='conference-talk'),
     (r'^conference/', include('conference.urls')),
     (r'^hcomments/', include('hcomments.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^p3/', include('p3.urls')),
 #    (r'^search/', include('haystack.urls')),
 )
-
-#if settings.DEBUG:
-#    import os.path
-#    args = []
-#    for k, path in settings.STATIC_DIRS.items():
-#        args.append((
-#            r'^static/%s/(?P<path>.*)$' % k,
-#            'django.views.static.serve',
-#            {'document_root': os.path.join(path, k), 'show_indexes': True}
-#        ))
-#    args.append((
-#        r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
-#        'django.views.static.serve',
-#        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
-#    ))
-#    from conference.settings import STUFF_URL, STUFF_DIR
-#    args.append((
-#        r'^%s(?P<path>.*)$' % STUFF_URL[1:],
-#        'django.views.static.serve',
-#        {'document_root': STUFF_DIR, 'show_indexes': True}
-#    ))
-#    urlpatterns += patterns('', *args)
 
 from pages import views as pviews
 # Questa view reimplementa il vecchio supporto di pages per le richieste ajax.
