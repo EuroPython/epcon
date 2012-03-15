080a55ff99fa9b34a1279bacf8e995e90320425a
David Mugnai <dvd@develer.com>
2012-03-15 12:09:18 +0100

    supporto per i tag field readonly

--- conference/templates/conference/init.js
+++ conference/templates/conference/init.js
@@ -31,6 +31,60 @@ function setup_conference_fields(ctx) {
             });
         }
     }
+    var tfields = $('.readonly-tag-field');
+    if(tfields.length && conference) {
+        var wrapper = $('<div class="all-tags"></div>');
+        var tags = [];
+        for(var key in conference.taggeditems) {
+            if(conference.taggeditems[key]['talk'] > 0) {
+                tags.push(key);
+            }
+        }
+        tags.sort();
+        for(var ix=0; ix<tags.length; ix++) {
+            var t = tags[ix];
+            var count = conference.taggeditems[t]['talk'];
+            var a = $('<a class="tag" href="#" data-tag="' + t + '">' + t + ' (' + count + ')' + '</a>');
+            a.click(function(e) {
+                var e = $(this);
+                var input = e.parent().prev();
+                var name = e.attr('data-tag');
+                var v = input.val();
+                if(v.indexOf(name) == -1) {
+                    v = '"' + name + '",' + v;
+                    e.addClass('selected');
+                }
+                else {
+                    var r = new RegExp('"' + name + '",?');
+                    v = v.replace(r, '');
+                    e.removeClass('selected');
+                }
+                if(v.substr(v.length-1) == ',')
+                    v = v.slice(0, -1);
+                input.val(v).change();
+                return false;
+            })
+            wrapper.append(a);
+        }
+        tfields
+            .not('[data-conference-init]')
+            .attr('data-conference-init', '1')
+            .after(wrapper)
+            .each(function() {
+                var tags = $(this).val().split(',');
+                $(tags).each(function(ix, v) {
+                    tags[ix] = v.replace(new RegExp('"', 'g'), '');
+                });
+                $(this)
+                    .next()
+                    .children('a.tag')
+                    .each(function() {
+                        var e = $(this);
+                        if(tags.indexOf(e.attr('data-tag')) != -1)
+                            e.addClass('selected');
+                    });
+            });
+    }
     var mfields = $('.markedit-widget', ctx);
     if(mfields.length) {
         mfields.markedit({
@@ -40,7 +94,7 @@ function setup_conference_fields(ctx) {
             }
         }).blur();
     }
-    $('.pseudo-radio').click(function(e) {
+    $('.pseudo-radio', ctx).click(function(e) {
         var pseudo = $(this);
         var p = pseudo.parent();
         var input = $('input[type=hidden]', p);
@@ -49,9 +103,10 @@ function setup_conference_fields(ctx) {
         $('.pseudo-radio', p).removeClass('checked');
         pseudo.addClass('checked');
     });
-    $('.pseudo-radio-field input').each(function() {
+    $('.pseudo-radio-field input', ctx).each(function() {
         var initial = $(this).val();
-        $('.pseudo-radio[data-value=' + initial + ']', $(this).parent()).click();
+        $('.pseudo-radio', $(this).parent()).removeClass('checked');
+        $('.pseudo-radio[data-value=' + initial + ']', $(this).parent()).addClass('checked');
     });
 }
 $(function() {
