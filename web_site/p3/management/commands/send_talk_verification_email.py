ae3354a057e1eebaaab8b8ed6d34c65885fd0a6b
David Mugnai <dvd@gnx.it>
2012-03-22 13:08:29 +0100

    Implementato il filtro per data
    Former-commit-id: d01377ada9f2275d337441b432f6c83eec86d050

--- web_site/p3/management/commands/send_talk_verification_email.py
+++ web_site/p3/management/commands/send_talk_verification_email.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from django.core.management.base import BaseCommand, CommandError
 from conference import models
+from datetime import datetime
 from email_template import utils
 
 class Command(BaseCommand):
@@ -20,9 +21,13 @@ class Command(BaseCommand):
 
         qs = models.TalkSpeaker.objects\
             .filter(talk__conference=conference)\
-            .select_related('talk', 'speaker__user')
+            .select_related('talk', 'speaker__user')\
+            .order_by('talk__created')
         if '@' in afilter:
             qs = qs.filter(speaker__user__email=afilter)
+        elif '/' in afilter:
+            d = datetime.strptime(afilter, '%Y/%m/%dT%H:%M:%S')
+            qs = qs.filter(talk__created__gt=d)
         # more filters here...
 
         data = []
@@ -40,7 +45,7 @@ class Command(BaseCommand):
                 tpl = 'verify-training-data'
             else:
                 raise ValueError('unknown talk type')
-            print email, '->', row.talk.title
+            print email, '->', row.talk.title, '(%s)' % row.talk.type, row.talk.created
             data.append((email, ctx, tpl))
 
         for email, ctx, tpl in data:
