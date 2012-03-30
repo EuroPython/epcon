9bc7f25cab6f9c03b2c04f928b13b5879a82a452
David Mugnai <dvd@develer.com>
2012-03-30 15:31:06 +0200

    piccoli fix alla generazione della classifica e commenti

--- conference/management/commands/ranking_of_talks.py
+++ conference/management/commands/ranking_of_talks.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand, CommandError
 from conference import models
 from conference import utils
 
+from collections import defaultdict
 from optparse import make_option
 
 class Command(BaseCommand):
@@ -20,12 +21,34 @@ class Command(BaseCommand):
         except IndexError:
             raise CommandError('conference not specified')
 
-        talks = models.Talk.objects.filter(conference=conference)
-        votes = models.VotoTalk.objects.filter(talk__in=talks)
-        users = set(x.user_id for x in votes)
+        talks = models.Talk.objects\
+            .filter(conference=conference, status='proposed')
         if options['show_input']:
             print utils._input_for_ranking_of_talks(talks)
         else:
-            print '%d talks / %d users / %d votes' % (talks.count(), len(users), len(votes))
-            for ix, t in enumerate(utils.ranking_of_talks(talks)):
-                print ix+1, '-', t.id, '-', t.title.encode('utf-8'), '(%s)' % (', '.join(s.name.encode('utf-8') for s in t.get_all_speakers()),)
+            qs = models.VotoTalk.objects\
+                .filter(talk__in=talks)\
+                .values('user')
+            votes = qs.count()
+            users = qs.distinct().count()
+            speakers = defaultdict(list)
+            sqs = models.TalkSpeaker.objects\
+                .filter(talk__in=talks)\
+                .values('talk', 'speaker__user__first_name', 'speaker__user__last_name')
+            for row in sqs:
+                name = '%s %s' % (row['speaker__user__first_name'], row['speaker__user__last_name'])
+                speakers[row['talk']].append(name.encode('utf-8'))
+
+            print '%d talks / %d users / %d votes' % (talks.count(), users, votes)
+            results = defaultdict(lambda: defaultdict(list))
+            for t in utils.ranking_of_talks(talks):
+                results[t.type][t.language].append(t)
+
+            for type, label in models.TALK_TYPE:
+                for language, subset in results[type].items():
+                    if subset:
+                        print label, '(%s)' % language
+                        print '-' * 79
+                        for ix, t in enumerate(subset):
+                            print ix+1, '-', t.id, '-', t.title.encode('utf-8'), '(%s)' % ', '.join(speakers[t.id])
+                        print ''
