68bd7f19669d19f2e82c2d7b882a80d27260d606
David Mugnai <dvd@develer.com>
2012-04-19 17:43:44 +0200

    utility function per calcolare il nome di una cache di un frammento html
    Former-commit-id: 7b352800538f86c79645b2a73c7f4ec324b5b6bf

--- web_site/p3/utils.py
+++ web_site/p3/utils.py
@@ -175,3 +175,18 @@ def gravatar(email, size=80, default='identicon', rating='r', protocol='https'):
         'rating': rating,
     })
     return gravatar_url
+
+
+from django.core.cache import cache
+from django.utils.http import urlquote
+from django.utils.hashcompat import md5_constructor
+
+def template_cache_name(fragment_name, *variables):
+    args = md5_constructor(u':'.join([urlquote(var) for var in variables]))
+    return 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
+
+def invalidate_template_cache(fragment_name, *variables):
+    args = md5_constructor(u':'.join([urlquote(var) for var in variables]))
+    cache_key = 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
+    cache.delete(cache_key)
+    return
