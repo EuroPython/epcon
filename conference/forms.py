6e819befe1fa25cad18fc27b941cfb35f39e1d39
David Mugnai <dvd@develer.com>
2012-06-29 15:53:51 +0200

    label più significativa

--- conference/forms.py
+++ conference/forms.py
@@ -486,4 +486,4 @@ sent to:
         )
 
 class AttendeeLinkDescriptionForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(label='A note to yourself (when you met this persone, why you want to stay in touch)', widget=forms.Textarea)
