3f03ffcb9875ad53e248b1265790ab3fdbc87e14
David Mugnai <dvd@gnx.it>
2013-01-11 22:51:19 +0100

    default per la static_root
    Former-commit-id: 3cf4813e8a99cc2d1fc0c27f09f672054c84fa24

--- pycon/settings.py
+++ pycon/settings.py
@@ -73,7 +73,7 @@ SECURE_MEDIA_URL = '/p3/secure_media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = DATA_DIR + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
