bea4207395b783b9d690c5f9f81f66b62ecd0fdc
David Mugnai <dvd@gnx.it>
2013-02-15 23:22:34 +0100

    fab per aggiornare la working copy di sviluppo
    Former-commit-id: 4151df7d7fceccf27c46256d7c951d9dae57840c

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import env, task, get
+from fabric.api import env, task, get, lcd, local
 from fabric.contrib import django
 
 import sys
@@ -27,3 +27,22 @@ def sync_media():
     remote_path = '/srv/europython/data/media_public'
     local_path = parent_(settings.MEDIA_ROOT)
     get(remote_path, local_path)
+
+@task
+def update():
+    venv = os.environ['VIRTUAL_ENV']
+    src = os.path.join(venv, 'src')
+    with lcd(src):
+        for f in os.listdir(src):
+            fpath = os.path.join(src, f)
+            if not os.path.isdir(fpath):
+                continue
+            with lcd(fpath):
+                print '--> git', fpath
+                local('git pull')
+    print '--> git .'
+    local('git pull')
+    print '--> syncdb + migrate'
+    local('./manage.py syncdb')
+    local('./manage.py migrate')
+
