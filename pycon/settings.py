6e12d91c5387939389da65e536799a911081a51e
David Mugnai <dvd@develer.com>
2013-05-31 11:35:43 +0200

    modificata l'email di default a cui si mandano i messaggi interni di assopy
    Former-commit-id: d1f991bfc5bc361f255f0a7c7c7a80505aac818c

--- pycon/settings.py
+++ pycon/settings.py
@@ -517,7 +517,7 @@ ASSOPY_VIES_WSDL_URL = None
 ASSOPY_BACKEND = 'http://assopy.pycon.it/conference/externalcall'
 ASSOPY_SEARCH_MISSING_USERS_ON_BACKEND = True
 ASSOPY_TICKET_PAGE = 'p3-tickets'
-ASSOPY_SEND_EMAIL_TO = CONFERENCE_SEND_EMAIL_TO
+ASSOPY_SEND_EMAIL_TO = ['log@pycon.it']
 ASSOPY_REFUND_EMAIL_ADDRESS = {
     'approve': ['info@pycon.it'],
     'execute': {
