61ab5962264f5956ac58681ff6b436c7da3c845e
David Mugnai <dvd@develer.com>
2013-05-31 12:11:08 +0200

    Spostate le viste con i report (sim, hotel) in un modulo a se stante
    Former-commit-id: d596d2774fc86809a4b8047186c5c7884fe6e297

--- p3/views/__init__.py
+++ p3/views/__init__.py
@@ -24,8 +24,6 @@ from email_template import utils
 
 import datetime
 import logging
-import mimetypes
-import os.path
 import uuid
 from collections import defaultdict
 
@@ -258,25 +256,6 @@ def user(request, token):
     auth.login(request, user)
     return HttpResponseRedirectSeeOther(reverse('p3-tickets'))
 
-def secure_media(request, path):
-    if not (request.user.is_superuser or request.user.groups.filter(name__in=('sim_report', 'hotel_report')).exists()):
-        fname = os.path.splitext(os.path.basename(path))[0]
-        if fname.rsplit('-', 1)[0] != request.user.username:
-            return http.HttpResponseForbidden()
-    fpath = settings.SECURE_STORAGE.path(path)
-    guessed = mimetypes.guess_type(fpath)
-    try:
-        r = http.HttpResponse(file(fpath), mimetype=guessed[0])
-        r['Content-Length'] = os.path.getsize(fpath)
-        if guessed[1]:
-            r['Content-Encoding'] = guessed[1]
-        return r
-    except IOError, e:
-        if e.errno == 2:
-            raise http.Http404()
-        else:
-            raise
-
 @login_required
 @render_to('p3/sprint_submission.html')
 def sprint_submission(request):
@@ -363,91 +342,6 @@ def sprint(request, sid):
             'attendees': attendees,
             'user_attend': request.user.id in set(x.user.id for x in attendees),
         },
-    }
-
-@login_required
-@render_to('p3/sim_report.html')
-def sim_report(request):
-    if not (request.user.is_superuser or request.user.groups.filter(name='sim_report').exists()):
-        return http.HttpResponseForbidden()
-    tickets = cmodels.Ticket.objects.filter(
-        orderitem__order___complete=True,
-        fare__in=cmodels.Fare.objects.filter(
-            code__startswith='SIM', conference=settings.CONFERENCE_CONFERENCE),
-    ).order_by('orderitem__order').select_related('p3_conference_sim')
-    if request.method == 'POST':
-        t = tickets.get(id=request.POST['ticket'])
-        t.frozen = not t.frozen
-        t.save()
-    compiled = dict(std=0, micro=0)
-    for t in tickets:
-        if t.p3_conference_sim and t.p3_conference_sim.document:
-            compiled[t.p3_conference_sim.sim_type] += 1
-    return {
-        'tickets': tickets,
-        'compiled': compiled,
-    }
-
-@login_required
-@render_to('p3/hotel_report.html')
-def hotel_report(request):
-    if not (request.user.is_superuser or request.user.groups.filter(name='hotel_report').exists()):
-        return http.HttpResponseForbidden()
-    tickets = models.TicketRoom.objects\
-        .valid_tickets()\
-        .select_related('ticket__fare', 'ticket__orderitem')\
-        .order_by('ticket__orderitem__order')
-    if request.method == 'POST':
-        t = tickets.get(id=request.POST['ticket'])
-        t.frozen = not t.frozen
-        t.save()
-    grouped = {
-        'rooms': {},
-        'beds': {},
-    }
-    omap = defaultdict(lambda: 0)
-    for t in tickets:
-        fcode = t.ticket.fare.code
-        if fcode[2] == '1':
-            key = '0single'
-        elif fcode[2] == '2':
-            key = '1double'
-        elif fcode[2] == '3':
-            key = '2triple'
-        elif fcode[2] == '4':
-            key = '3quadruple'
-        if fcode[1] == 'R':
-            g = grouped['rooms']
-            if key not in g:
-                g[key] = {}
-
-            # oid/order_key servono solo a raggruppare graficamente i biglietti
-            # per la stessa camera
-            oid = '%s:%s' % (t.ticket.orderitem.order_id, fcode)
-            order_key = oid + ('_%d' % (omap[oid] / int(fcode[2])))
-            omap[oid] += 1
-            if order_key not in g[key]:
-                g[key][order_key] = []
-            g[key][order_key].append(t)
-        else:
-            g = grouped['beds']
-            if key not in g:
-                g[key] = {}
-            period = (t.checkin, t.checkout)
-            if period not in g[key]:
-                g[key][period] = []
-            g[key][period].append(t)
-
-    rooms = []
-    for type, orders in sorted(grouped['rooms'].items()):
-        rooms.append((type[1:], sorted(orders.items(), key=lambda x: (x[1][0].checkin, x[1][0].ticket.name))))
-
-    beds = []
-    for type, periods in sorted(grouped['beds'].items()):
-        beds.append((type[1:], sorted(periods.items(), key=lambda x: x[0][0])))
-    return {
-        'rooms': rooms,
-        'beds': beds,
     }
 
 def whos_coming(request, conference=None):
@@ -691,3 +585,4 @@ def genro_invoice_pdf(request, assopy_id):
 from p3.views.cart import *
 from p3.views.profile import *
 from p3.views.schedule import *
+from p3.views.reports import *
---
+++ p3/views/reports.py
@@ -0,0 +1,114 @@
+# -*- coding: UTF-8 -*-
+from conference import models as cmodels
+from collections import defaultdict
+from django import http
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from p3 import models
+import mimetypes
+import os.path
+
+def secure_media(request, path):
+    if not (request.user.is_superuser or request.user.groups.filter(name__in=('sim_report', 'hotel_report')).exists()):
+        fname = os.path.splitext(os.path.basename(path))[0]
+        if fname.rsplit('-', 1)[0] != request.user.username:
+            return http.HttpResponseForbidden()
+    fpath = settings.SECURE_STORAGE.path(path)
+    guessed = mimetypes.guess_type(fpath)
+    try:
+        r = http.HttpResponse(file(fpath), mimetype=guessed[0])
+        r['Content-Length'] = os.path.getsize(fpath)
+        if guessed[1]:
+            r['Content-Encoding'] = guessed[1]
+        return r
+    except IOError, e:
+        if e.errno == 2:
+            raise http.Http404()
+        else:
+            raise
+
+@login_required
+def sim_report(request):
+    if not (request.user.is_superuser or request.user.groups.filter(name='sim_report').exists()):
+        return http.HttpResponseForbidden()
+    tickets = cmodels.Ticket.objects.filter(
+        orderitem__order___complete=True,
+        fare__in=cmodels.Fare.objects.filter(
+            code__startswith='SIM', conference=settings.CONFERENCE_CONFERENCE),
+    ).order_by('orderitem__order').select_related('p3_conference_sim')
+    if request.method == 'POST':
+        t = tickets.get(id=request.POST['ticket'])
+        t.frozen = not t.frozen
+        t.save()
+    compiled = dict(std=0, micro=0)
+    for t in tickets:
+        if t.p3_conference_sim and t.p3_conference_sim.document:
+            compiled[t.p3_conference_sim.sim_type] += 1
+    ctx = {
+        'tickets': tickets,
+        'compiled': compiled,
+    }
+    return render(request, 'p3/sim_report.html', ctx)
+
+@login_required
+def hotel_report(request):
+    if not (request.user.is_superuser or request.user.groups.filter(name='hotel_report').exists()):
+        return http.HttpResponseForbidden()
+    tickets = models.TicketRoom.objects\
+        .valid_tickets()\
+        .select_related('ticket__fare', 'ticket__orderitem')\
+        .order_by('ticket__orderitem__order')
+    if request.method == 'POST':
+        t = tickets.get(id=request.POST['ticket'])
+        t.frozen = not t.frozen
+        t.save()
+    grouped = {
+        'rooms': {},
+        'beds': {},
+    }
+    omap = defaultdict(lambda: 0)
+    for t in tickets:
+        fcode = t.ticket.fare.code
+        if fcode[2] == '1':
+            key = '0single'
+        elif fcode[2] == '2':
+            key = '1double'
+        elif fcode[2] == '3':
+            key = '2triple'
+        elif fcode[2] == '4':
+            key = '3quadruple'
+        if fcode[1] == 'R':
+            g = grouped['rooms']
+            if key not in g:
+                g[key] = {}
+
+            # oid/order_key servono solo a raggruppare graficamente i biglietti
+            # per la stessa camera
+            oid = '%s:%s' % (t.ticket.orderitem.order_id, fcode)
+            order_key = oid + ('_%d' % (omap[oid] / int(fcode[2])))
+            omap[oid] += 1
+            if order_key not in g[key]:
+                g[key][order_key] = []
+            g[key][order_key].append(t)
+        else:
+            g = grouped['beds']
+            if key not in g:
+                g[key] = {}
+            period = (t.checkin, t.checkout)
+            if period not in g[key]:
+                g[key][period] = []
+            g[key][period].append(t)
+
+    rooms = []
+    for type, orders in sorted(grouped['rooms'].items()):
+        rooms.append((type[1:], sorted(orders.items(), key=lambda x: (x[1][0].checkin, x[1][0].ticket.name))))
+
+    beds = []
+    for type, periods in sorted(grouped['beds'].items()):
+        beds.append((type[1:], sorted(periods.items(), key=lambda x: x[0][0])))
+    ctx = {
+        'rooms': rooms,
+        'beds': beds,
+    }
+    return render(request, 'p3/hotel_report.html', ctx)
