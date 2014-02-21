72ac68b6004e75235b062a472d99a1b5becfd3ce
David Mugnai <dvd@gnx.it>
2014-02-21 01:13:17 +0100

    cfp/form: riagganciata la form al backend

--- pycon/settings.py
+++ pycon/settings.py
@@ -136,9 +136,7 @@ ROOT_URLCONF = 'pycon.urls'
 WSGI_APPLICATION = 'pycon.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
