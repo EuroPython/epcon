886fa691440f5f39b1d934cda057493c3d10a5c4
David Mugnai <dvd@develer.com>
2014-03-04 16:44:02 +0100

    locale middleware disattivato per impedire il fallback sulla lingua supportata dal browser

--- pycon/settings.py
+++ pycon/settings.py
@@ -124,9 +124,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    #'django.middleware.locale.LocaleMiddleware',
     'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
     'assopy.middleware.DebugInfo',
 )
