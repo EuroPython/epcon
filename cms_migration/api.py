802df34dbfc5234692a966fb8faadcc81e940e02
Iacopo Spalletti <i.spalletti@nephila.it>
2014-10-26 18:19:51 +0100

    Rename content migration settings

--- cms_migration/api.py
+++ cms_migration/api.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
 from cms.api import add_plugin
 from cms.models import Page as CMSPage, Title
 from pages.models import Page as PagesPage
@@ -8,12 +7,16 @@ from pages.models import Page as PagesPage
 def copy_pages():
 
     pages = PagesPage.objects.order_by('tree_id', 'lft', )
+    # remove all the previous pages
     CMSPage.objects.all().delete()
 
     parents = {}
 
     for page in pages:
         cmspage = CMSPage()
+        # replicate the language independent structure information
+        # thanks to the common ancestor we can reuse exactly the same tree
+        # information
         cmspage.pk = page.pk
         cmspage.creation_date = page.creation_date
         cmspage.publication_date = page.publication_date
@@ -30,6 +33,7 @@ def copy_pages():
         cmspage.save(no_signals=True)
         cmspage.rescan_placeholders()
         parents[cmspage.pk] = cmspage
+        # replicate the language dependent data
         for language in page.get_languages():
             cmstitle = Title()
             cmstitle.language = language
@@ -40,8 +44,17 @@ def copy_pages():
             cmstitle.save()
             contents = page.content_by_language(language)
             for content in contents:
-                if content.type not in ('slug', 'title'):
-                    print(content.type, cmspage.template)
+                if content.type in ('subtitle', 'text', 'right-column'):
                     placeholder = cmspage.placeholders.get(slot=content.type)
-                    print(placeholder)
+                    # For the main placeholders we need to scan whether content
+                    # is markdown or plain HTML
+                    # This is a very naif check, but does the trick in this
+                    # context
+                    if content.body.find('<p>') > -1:
+                        add_plugin(placeholder, plugin_type='TextPlugin', language=language, body=content.body)
+                    else:
+                        add_plugin(placeholder, plugin_type='MarkItUpPlugin', language=language, body=content.body)
+                elif content.type not in ('slug', 'title'):
+                    # Fallback import
+                    placeholder = cmspage.placeholders.get(slot=content.type)
                     add_plugin(placeholder, plugin_type='TextPlugin', language=language, body=content.body)
