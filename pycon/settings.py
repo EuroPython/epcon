dad00b9e3da8b871c303467eadb4d8c57707fd57
Christian Barra <barrachri@gmail.com>
2015-01-22 11:41:46 +0100

    set only english

--- pycon/settings.py
+++ pycon/settings.py
@@ -43,11 +43,12 @@ TIME_ZONE = 'Europe/Rome'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'it'
+LANGUAGE_CODE = 'en'
 
 ugettext = lambda s: s
 LANGUAGES = (
-    ('it', ugettext('Italiano')),
+    # disabled italian
+    #('it', ugettext('Italiano')),
     ('en', ugettext('English')),
 )
 
