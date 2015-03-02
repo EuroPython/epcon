1da2877a59f02bf59e357ba711b5b1c71c2fb393
Oier <badtrex@gmail.com>
2015-03-02 15:06:50 +0100

    Update settings.py

--- conference/settings.py
+++ conference/settings.py
@@ -30,7 +30,7 @@ FORMS.update(getattr(settings, 'CONFERENCE_FORMS', {}))
 
 import os
 #MAX_TICKETS =  os.environ.get("MAX_TICKETS")
-MAX_TICKETS = 100
+MAX_TICKETS = 350
 
 # url a cui rimandare un utente che prova ad accedere alla paper submission
 # quando il cfp è chiuso. Se None viene ritornato un 404
