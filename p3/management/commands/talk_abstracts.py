7236446fa87a78fadee22997f7782bc94eae6793
Alexandre M. Savio <alexsavio@gmail.com>
2016-03-01 23:44:24 +0100

    update talk_abstracts command

--- p3/management/commands/talk_abstracts.py
+++ p3/management/commands/talk_abstracts.py
@@ -3,37 +3,36 @@
 
 """
 from   django.core.management.base import BaseCommand, CommandError
-from   django.core  import urlresolvers
 from   conference   import models
-from   conference   import utils
 
 from   p3           import models as p3_models
 from   assopy       import models as assopy_models
 
-from   collections  import defaultdict, OrderedDict
+from   collections  import OrderedDict
 from   optparse     import make_option
-import operator
 import simplejson   as json
 import traceback
 
 ### Globals
 VERBOSE = False
+#
+# TYPE_NAMES = (
+#     ('keynote', 'Keynotes'),
+#     ('s', 'Talks'),
+#     ('t', 'Trainings'),
+#     ('p', 'Poster sessions'),
+#     ('h', 'Help desks'),
+#     ('europython', 'EuroPython sessions'),
+#     ('i', 'Other sessions'),
+#     )
+#
+#
+# def _check_talk_types(type_names):
+#     d = dict(type_names)
+#     for code, entry in models.TALK_TYPE:
+#         assert code in d, 'Talk type code %r is missing' % code
+# _check_talk_types(TYPE_NAMES)
 
-TYPE_NAMES = (
-    ('keynote', 'Keynotes'),
-    ('s', 'Talks'),
-    ('t', 'Trainings'),
-    ('p', 'Poster sessions'),
-    ('h', 'Help desks'),
-    ('europython', 'EuroPython sessions'),
-    ('i', 'Other sessions'),
-    )
-
-def _check_talk_types(type_names):
-    d = dict(type_names)
-    for code, entry in models.TALK_TYPE:
-        assert code in d, 'Talk type code %r is missing' % code
-_check_talk_types(TYPE_NAMES)
 
 ### Helpers
 def speaker_listing(talk):
@@ -45,11 +44,14 @@ def speaker_emails(talk):
     return u', '.join(
         u'{}'.format(speaker.user.email) for speaker in talk.get_all_speakers())
 
+
 def get_orders_from(user):
     return assopy_models.Order.objects.filter(_complete=True, user=user.id)
 
+
 def get_tickets_assigned_to(user):
     return p3_models.TicketConference.objects.filter(assigned_to=user.email)
+
 
 def is_ticket_assigned_to_someone_else(ticket, user):
     tickets = p3_models.TicketConference.objects.filter(ticket_id=ticket.id)
@@ -144,15 +146,16 @@ def talk_schedule(talk):
     timerange = event.get_time_range()
     return '{}, {}'.format(str(timerange[0]), str(timerange[1]))
 
-###
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
-        make_option('--verbose',
-             action='store_true',
-             dest='verbose',
-             default=False,
-             help='Verbose will print further check results on the status of talks.',
+        make_option('--talk_status',
+             action='store',
+             dest='talk_status',
+             default='proposed',
+             choices=['accepted', 'proposed'],
+             help='The status of the talks to be put in the report. '
+                  'Choices: accepted, proposed',
         ),
         # make_option('--option',
         #     action='store',
@@ -162,53 +165,64 @@ class Command(BaseCommand):
         #     help='Help text',
         # ),
     )
+
     def handle(self, *args, **options):
         try:
             conference = args[0]
         except IndexError:
             raise CommandError('conference not specified')
 
-        if options['verbose']:
-            VERBOSE = True
+        # Group by admin types
+        talks = OrderedDict()
+        for adm_type, type_name in dict(models.TALK_ADMIN_TYPE).items():
+            talks[type_name] = (models.Talk.objects
+                                   .filter(conference=conference,
+                                           status=options['talk_status'],
+                                           admin_type=adm_type))
 
-        talks = (models.Talk.objects
-                 .filter(conference=conference,
-                         status='accepted'))
+        type_groups = {'talk':        ['t_30', 't_45', 't_60'],
+                       'interactive': ['i_60'],
+                       'training':    ['r_180'],
+                       'panel':       ['p_60', 'p_90'],
+                       'poster':      ['p_180'],
+                       'helpdesk':    ['h_180'],
+                      }
+        for grp_name, grp_types in type_groups.items():
+            grp_talks = []
+            for talk_type in grp_types:
+                bag = (models.Talk.objects
+                         .filter(conference=conference,
+                                 status=options['talk_status'],
+                                 type=talk_type,
+                                 admin_type=''))
+                grp_talks.extend(bag)
 
-        # Group by types
-        talk_types = {}
-        for talk in talks:
-            if 'EPS' in talk.title or 'EuroPython 20' in talk.title:
-                type = 'europython'
-            elif talk.title.lower().startswith('keynote'):
-                type = 'keynote'
-            else:
-                type = talk.type
-            if type in talk_types:
-                talk_types[type].append(talk)
-            else:
-                talk_types[type] = [talk]
+            talks[grp_name] = grp_talks
 
         sessions = OrderedDict()
         # Print list of submissions
-        for type, type_name in TYPE_NAMES:
-            bag = talk_types.get(type, [])
-            if not bag:
+        for type_name, session_talks in talks.items():
+            if not session_talks:
                 continue
 
             sessions[type_name] = OrderedDict()
 
             # Sort by talk title using title case
-            bag.sort(key=lambda talk: talk_title(talk).title())
-            for talk in bag:
+            session_talks.sort(key=lambda talk: talk_title(talk).title())
+            for talk in session_talks:
 
                 sessions[type_name][talk.id] = {
                 'id':           talk.id,
+                'adm_type':     talk.get_admin_type_display().encode('utf-8'),
+                'type':         talk.get_type_display().encode('utf-8'),
                 'duration':     talk.duration,
+                'level':        talk.get_level_display().encode('utf-8'),
                 'track_title':  talk_track_title(talk).encode('utf-8'),
                 'timerange':    talk_schedule(talk).encode('utf-8'),
                 'tags':         [str(t) for t in talk.tags.all()],
                 'title':        talk_title(talk).encode('utf-8'),
+                'status':       talk.status.encode('utf-8'),
+                'language':     talk.get_language_display().encode('utf-8'),
                 'speakers':     speaker_listing(talk).encode('utf-8'),
                 'emails':       speaker_emails(talk).encode('utf-8'),
                 'have_tickets': have_tickets(talk),
