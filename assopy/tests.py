3b47a62f47a343c90457fe233be172558695173c
Stephane Wirtel <stephane@wirtel.be>
2017-02-13 13:16:13 +0100

    [WIP] Add stripe_charge_id in the Order model [WIP] Add a test for the stripe_charge_id [WIP] Add new requirements for the tests
    just execute python manage.py test

--- assopy/tests.py
+++ assopy/tests.py
@@ -4,20 +4,50 @@ unittest). These will both pass when you run "manage.py test".
 
 Replace these with more appropriate tests for your application.
 """
+import mock
+from django.test import TestCase, SimpleTestCase
+from assopy.models import User, Order
+from django.contrib.auth.models import User as AuthUser
+from django.core.urlresolvers import reverse
 
-from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class AddStripeInOrderTest(TestCase):
+    def test_stripe(self):
+        data = {
+            'stripeToken': '1234567890',
+            'stripeEmail': 'demo@demo.org',
+            'stripeTokenType': '1111',
+        }
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        auth_user = AuthUser.objects.create_user('admin', 'admin@demo.org', 'demo')
+        auth_user.save()
 
->>> 1 + 1 == 2
-True
-"""}
+        response = self.client.login(username='admin', password='demo')
 
+        self.assertTrue(response)
+
+        user = User.objects.create(user=auth_user)
+
+        class Charge(object):
+            id = '1'
+
+        with mock.patch('assopy.stripe.views.StripeCheckoutView.get_object') as get_object:
+            with mock.patch('stripe.Charge.create') as create:
+                with mock.patch('email_template.utils.email') as email:
+                    from django.core.mail import EmailMessage
+                    email.return_value = EmailMessage()
+
+                    order = Order.objects.create(user=user,
+                                                 payment='cc',  ## cc because stripe is a credit card.
+                                                 items=[])
+
+                    get_object.return_value = order
+                    create.return_value = Charge()
+
+                    url = reverse('assopy-stripe-checkout', kwargs={'pk': 12345})
+                    response = self.client.post(url, data=data, follow=True)
+                    # print(response)
+                    self.assertEqual(response.status_code, 200)
+
+                    order = Order.objects.get(id=order.id)
+                    self.assertEqual(order.stripe_charge_id, Charge.id)
