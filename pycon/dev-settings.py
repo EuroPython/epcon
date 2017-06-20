fad9a56216928c81aea2eb01de23345ec658499c
Stephane Wirtel <stephane@wirtel.be>
2017-06-20 19:04:27 +0200

    Add new dependencies for the development

--- pycon/dev-settings.py
+++ pycon/dev-settings.py
@@ -7,4 +7,21 @@ PAYPAL_TEST = True
 
 TEMPLATES[0]['OPTIONS']['debug'] = True
 
-INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',)
+INSTALLED_APPS = INSTALLED_APPS + (
+    'django_extensions',
+    'django_pdb',
+    'test_without_migrations',
+)
+
+#TEST_WITHOUT_MIGRATIONS_COMMAND = 'django_nose.management.commands.test.Command'
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'django_pdb.middleware.PdbMiddleware',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/tmp/p3.db',
+    }
+}
