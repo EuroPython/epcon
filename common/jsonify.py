3bfd91e78d6cf6f6efd0ffc19b73cafbd98eab3f
Stephane Wirtel <stephane@wirtel.be>
2017-07-04 08:53:41 +0200

    Improve the tests for common/jsonify

--- common/jsonify.py
+++ common/jsonify.py
@@ -1,22 +1,21 @@
+import datetime
+import decimal
 import functools
+
 import simplejson
+
 
 class MyEncode(simplejson.JSONEncoder):
     def default(self, obj):
-        import datetime, decimal
         if isinstance(obj, datetime.datetime):
             return obj.strftime('%d/%m/%Y %H:%M:%S')
         elif isinstance(obj, datetime.date):
             return obj.strftime('%d/%m/%Y')
         elif isinstance(obj, datetime.time):
             return obj.strftime('%H:%M')
-        elif isinstance(obj, decimal.Decimal):
-            return str(obj)
         elif isinstance(obj, set):
             return list(obj)
 
         return simplejson.JSONEncoder.default(self, obj)
 
 json_dumps = functools.partial(simplejson.dumps, cls=MyEncode)
-
-
--- common/tests/test_jsonify.py
+++ common/tests/test_jsonify.py
@@ -1,9 +1,11 @@
+import decimal
 import unittest
 import datetime
 
 import simplejson
 
 from common.jsonify import json_dumps
+
 
 class MyEncodeTestCase(unittest.TestCase):
     def test_datetime(self):
@@ -24,4 +26,37 @@ class MyEncodeTestCase(unittest.TestCase):
 
         self.assertDictEqual(simplejson.loads(json_dumps(obj)), {
             'date': now.strftime('%d/%m/%Y')
+        })
+
+    def test_time(self):
+        now = datetime.datetime.now().time()
+
+        obj = {
+            'time': now,
+        }
+
+        self.assertDictEqual(simplejson.loads(json_dumps(obj)), {
+            'time': now.strftime('%H:%M')
+        })
+
+    def test_set(self):
+        value = set([1, 2, 3])
+
+        obj = {
+            'set': value,
+        }
+
+        self.assertDictEqual(simplejson.loads(json_dumps(obj)), {
+            'set': [1, 2, 3],
+        })
+
+    def test_dict(self):
+        value = {'a': 1, 'b': 2}
+
+        obj = {
+            'dict': value,
+        }
+
+        self.assertDictEqual(simplejson.loads(json_dumps(obj)), {
+            'dict': value,
         })
