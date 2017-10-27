9c4838a0b5dbd3afd6b03476f3b34d32cff16d49
Artur Czepiel <czepiel.artur@gmail.com>
2017-10-27 11:09:14 +0200

    Change location of password_reset_email
    Extension changed from .html to .txt because it's a regular text email, and
    both filenames work exactly the same with django.

--- assopy/urls.py
+++ assopy/urls.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+from __future__ import unicode_literals, absolute_import
 
 from django.conf import settings as dsettings
 from django.conf.urls import url, include
@@ -12,8 +15,8 @@ from django.contrib.auth.views import (
     password_reset_complete,
 )
 
-
 from assopy.forms import LoginForm, SetPasswordForm
+
 
 urlpatterns = [
     url(r'^login/$',
