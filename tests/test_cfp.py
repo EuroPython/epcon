0ab878a7e2a64eeaa3abbe5ae48038b900dece3f
Artur Czepiel <czepiel.artur@gmail.com>
2018-02-27 12:43:01 +0100

    add some basic tests for CFP submission;
     bump coverage fail_below to 40%;
     added couple of ignores to .flake8

---
+++ tests/test_cfp.py
@@ -0,0 +1,152 @@
+# coding: utf-8
+
+from datetime import timedelta
+from httplib import (
+    OK as HTTP_OK_200,
+    NOT_FOUND as HTTP_NOT_FOUND_404,
+    FOUND as HTTP_REDIRECT_302
+)
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.utils import timezone
+
+from django_factory_boy import auth as auth_factories
+
+from assopy.tests.factories.user import UserFactory as AssopyUserFactory
+from conference.models import Conference, Talk
+
+
+# TODO(artcz) add tests for CMS-based CFP page that redirects to the submission
+# page tested below
+
+
+# Using django's TestCase because of assertRedirects
+# however we still run it using pytest
+class TestCFP(TestCase):
+    """
+    Test everything related to CFP
+    """
+
+    def setUp(self):
+        self.conference = Conference.objects.create(
+            code=settings.CONFERENCE_CONFERENCE,
+            name=settings.CONFERENCE_CONFERENCE,
+            # by default start with open CFP
+            cfp_start=timezone.now() - timedelta(days=2),
+            cfp_end=timezone.now()   + timedelta(days=2)
+        )
+
+        # default password is 'password123' per django_factory_boy
+        self.user = auth_factories.UserFactory(email='joedoe@example.com',
+                                               is_active=True)
+        AssopyUserFactory(user=self.user)
+
+        self.form_url = reverse("conference-paper-submission")
+
+    def test_login_required(self):
+        login_url = reverse('login') + '?next=' + self.form_url
+        response = self.client.get(self.form_url)
+        assert response.status_code == HTTP_REDIRECT_302
+        self.assertRedirects(response, login_url)
+
+    def test_accessing_cfp_form_before_CFP_is_opened(self):
+        self.conference.cfp_start = timezone.now() + timedelta(days=2)
+        self.conference.save()
+        self.client.login(email='joedoe@example.com', password='password123')
+
+        # TODO(artcz) - this test is correct but I'd expect a different
+        # behaviour than 404 here. Something like a template saying "Sorry CFP
+        # is not yet opened"
+        response = self.client.get(self.form_url, follow=True)
+        assert response.status_code == HTTP_NOT_FOUND_404
+
+    def test_accessing_cfp_form_after_CFP_is_closed(self):
+        self.conference.cfp_end = timezone.now() - timedelta(days=1)
+        self.conference.save()
+        self.client.login(email='joedoe@example.com', password='password123')
+
+        # TODO(artcz) - this test is correct but I'd expect a different
+        # behaviour than 404 here. Something like a template saying "Sorry CFP
+        # is closed"
+        response = self.client.get(self.form_url, follow=True)
+        assert response.status_code == HTTP_NOT_FOUND_404
+
+    def test_accessing_cfp_form_while_cfp_is_live(self):
+        self.client.login(email='joedoe@example.com', password='password123')
+
+        response = self.client.get(self.form_url, follow=True)
+        assert response.status_code == HTTP_OK_200
+        self.assertTemplateUsed(response, "conference/paper_submission.html")
+
+    def test_post_some_proposals(self):
+        assert Talk.objects.all().count() == 0
+        self.client.login(email='joedoe@example.com', password='password123')
+
+        VALIDATION_FAILED_200     = HTTP_OK_200
+        VALIDATION_SUCCESSFUL_303 = 303
+
+        required_fields = ['bio', 'first_name', 'last_name', 'abstract_short',
+                           'abstract', 'level', 'slides_agreement', 'phone',
+                           'birthday', 'personal_agreement', 'title', 'type',
+                           'tags', 'video_agreement']
+
+        def required_fields_not_filled(response, fields):
+            """
+            Helper function
+            """
+            errors = response.context['form'].errors
+
+            for f in fields:
+                assert errors[f] == ['This field is required.']
+                del errors[f]
+
+            # we make sure there are no other errors in the form
+            assert errors == {}
+
+        empty_submission = {}
+        response = self.client.post(self.form_url, empty_submission)
+        assert response.status_code == VALIDATION_FAILED_200
+        required_fields_not_filled(response, required_fields)
+
+        dummy_data_in_all_required_fields = {f: "foo" for f in required_fields}
+        response = self.client.post(self.form_url,
+                                    dummy_data_in_all_required_fields)
+        assert response.status_code == VALIDATION_FAILED_200
+
+        # birthday, type and level require specific data
+        assert {'birthday', 'type', 'level'} ==\
+            set(response.context['form'].errors.keys())
+
+        talk_proposal = {
+            "type": "t_30",
+            'first_name': 'Joe',
+            'last_name': 'Doe',
+            "birthday": "2018-02-26",
+            'bio': "Python developer",
+            "title": "Testing EPCON CFP",
+            "abstract_short": "Short talk about testing CFP",
+            "abstract": "Using django TestCase and pytest",
+            "level": "advanced",
+            "phone": "41331237",
+            "tags": "django, testing, slides",
+            "personal_agreement": True,
+            "slides_agreement": True,
+            "video_agreement": True,
+        }
+
+        profile_url = reverse("conference-myself-profile")
+        response = self.client.post(self.form_url, talk_proposal)
+        assert response.status_code == VALIDATION_SUCCESSFUL_303
+        self.assertRedirects(response, profile_url,
+                             status_code=303, fetch_redirect_response=False)
+        talk_url = Talk.objects.get().get_absolute_url()
+        assert talk_url == "/conference/talks/testing-epcon-cfp"
+
+        # At last check if the talk is visible in the list of submitted talks
+        response = self.client.get(profile_url, follow=True)
+        assert response.status_code == HTTP_OK_200
+        self.assertTemplateUsed(response, 'conference/profile.html')
+        self.assertContains(response, talk_proposal['title'])
+        self.assertContains(response, talk_url)
