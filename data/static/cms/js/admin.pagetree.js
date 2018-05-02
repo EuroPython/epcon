7abf6d7d291c3e2988617dd569cce9a118138f78
Artur Czepiel <czepiel.artur@gmail.com>
2018-05-02 14:46:24 +0200

    more removing statics

--- data/static/cms/js/admin.pagetree.js
+++
@@ -1,9 +0,0 @@
-// polyfills
-require('./polyfills/function.prototype.bind.js');
-require('./libs/pep');
-
-var CMS = require('./modules/cms.base');
-
-window.CMS = CMS;
-
-CMS.PageTree = require('./modules/cms.pagetree');
