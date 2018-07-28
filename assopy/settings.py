25769427cf5ca1f50b4746666566ad43a7d177a3
Agustin Mendez <matagus@gmail.com>
2018-07-28 15:00:41 +0100

    Removed janrain references, useless code & CSS stylesheets

--- assopy/settings.py
+++ assopy/settings.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-if hasattr(settings, 'ASSOPY_JANRAIN'):
-    JANRAIN = {
-        'domain': settings.ASSOPY_JANRAIN['domain'],
-        'app_id': settings.ASSOPY_JANRAIN['app_id'],
-        'secret': settings.ASSOPY_JANRAIN['secret'],
-    }
-else:
-    JANRAIN = None
-
 try:
     BACKEND = settings.ASSOPY_BACKEND
 except AttributeError:
