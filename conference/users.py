782da0c164652a309f9819f00b6e5ebea01ebf1a
Artur Czepiel <czepiel.artur@gmail.com>
2018-09-24 17:02:25 +0200

    replaced random username algo

--- conference/users.py
+++ conference/users.py
@@ -5,17 +5,13 @@ This module takes care of a lot of user related things.
 It's a good place to put validators and user management functions/classes
 """
 
-import hashlib
-import uuid
+import os
+
 
 RANDOM_USERNAME_LENGTH = 10
 
 
 def generate_random_username():
-    """Returns md5 of uuid that can be later used as a username
-
-    Using md5 instead of raw uuid so that it's later not confusing when we
-    introduce proper uuids for Users. We also don't need that many options so
-    we can just grab first few characters
-    """
-    return hashlib.md5(str(uuid.uuid4())).hexdigest()[:RANDOM_USERNAME_LENGTH]
+    """Returns random username of length set by RANDOM_USERNAME_LENGTH"""
+    # TODO: this will break on python3.
+    return os.urandom(100).encode('hex')[:RANDOM_USERNAME_LENGTH]
