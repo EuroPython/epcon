80727834e43b4193742753ef90e4a2f8e02d4d32
Artur Czepiel <czepiel.artur@gmail.com>
2018-12-14 21:40:34 +0100

    fix the fix for debug in jinja2

--- pycon/jinja2.py
+++ pycon/jinja2.py
@@ -9,7 +9,8 @@ def environment(**options):
     # TODO(artcz)
     # there is some bug with pytest that passes debug as argument here and
     # jinja can't work it out. poping for now.
-    options.pop('debug')
+    if 'debug' in options:
+        options.pop('debug')
 
     env = Environment(**options)
     env.globals.update({
