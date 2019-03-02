7f5229c3c8487eb989d16d6d4d1a5f84d597b31b
Artur Czepiel <czepiel.artur@gmail.com>
2019-03-02 19:01:16 +0100

    bootstrap4 poc

--- pycon/settings.py
+++ pycon/settings.py
@@ -141,11 +141,9 @@ STATIC_ROOT = DATA_DIR + '/static/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'assets'),
+]
 
 # List of finder classes that know how to find static files in
 # various locations.
