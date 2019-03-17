60213a5f42b3949e02ae7f5b6744bad48acca91b
Artur Czepiel <czepiel.artur@gmail.com>
2019-03-17 11:07:02 +0100

    fixed tests

--- tests/test_reset_password.py
+++ tests/test_reset_password.py
@@ -1,7 +1,3 @@
-# coding: utf-8
-
-
-
 from pytest import mark
 
 from django.core import mail
@@ -20,51 +16,56 @@ def test_reset_password(client):
     password.
     """
 
-    url = reverse("password_reset")
-    assert url == "/accounts/password_reset/"
+    url = reverse("accounts:password_reset")
+    assert url == "/accounts/password-reset/"
     response = client.get(url)
-    assert template_used(response, 'registration/password_reset_form.html')
+    assert template_used(response, "ep19/bs/accounts/password_reset.html")
     # make sure that we're not using default template from django admin
-    assert 'Django Administration' not in response.content.decode('utf-8')
-    assert 'input type="email" name="email" maxlength="254" required id="id_email"' in response.content.decode('utf-8')
+    assert "Django Administration" not in response.content.decode("utf-8")
+    assert 'input type="email"' in response.content.decode("utf-8")
 
     # --------
-    response = client.post(url, {'email': 'joedoe@example.com'})
+    response = client.post(url, {"email": "joedoe@example.com"})
     # successful redirect, but no email sent because user doesn't exist
     assert response.status_code == 302
-    assert response.url.endswith("/accounts/password_reset/done/")
+    assert response.url.endswith("/accounts/password-reset/done/")
     assert len(mail.outbox) == 0
 
     # --------
-    auth_factories.UserFactory(email='joedoe@example.com')
+    auth_factories.UserFactory(email="joedoe@example.com")
 
-    response = client.post(url, {'email': 'joedoe@example.com'})
+    response = client.post(url, {"email": "joedoe@example.com"})
     # successful redirect, and one email sent because user exists
     assert response.status_code == 302
-    assert response.url.endswith("/accounts/password_reset/done/")
+    assert response.url.endswith("/accounts/password-reset/done/")
     assert len(mail.outbox) == 1
 
-    response = client.get(reverse("password_reset_done"))
-    assert template_used(response, 'registration/password_reset_done.html')
+    response = client.get(reverse("accounts:password_reset_done"))
+    assert template_used(response, "ep19/bs/accounts/password_reset_done.html")
 
     # --------
     email = mail.outbox[0]
-    assert email.to == ['joedoe@example.com']
-    assert email.subject == 'Password reset on example.com'
+    assert email.to == ["joedoe@example.com"]
+    assert email.subject == "EuroPython2019: Reset password link"
     # get a relative url from the middle of the email.
-    url_from_email = email.body.splitlines()[5].split('example.com')[1]
+    url_from_email = email.body.splitlines()[6].split("example.com")[1]
 
     response = client.get(url_from_email)
     # This should be a template with two password inputs
-    assert template_used(response, 'registration/password_reset_confirm.html')
-    assert 'Django Administration' not in response.content.decode('utf-8')
-    assert 'name="new_password1"' in response.content.decode('utf-8')
-    assert 'name="new_password2"' in response.content.decode('utf-8')
+    assert template_used(
+        response, "ep19/bs/accounts/password_reset_confirm.html"
+    )
+    assert "Django Administration" not in response.content.decode("utf-8")
+    assert 'name="new_password1"' in response.content.decode("utf-8")
+    assert 'name="new_password2"' in response.content.decode("utf-8")
 
     # --------
-    response = client.post(url_from_email, {
-        'new_password1': 'asdf',
-        'new_password2': 'asdf',
-    }, follow=True)
-    assert template_used(response, 'registration/password_reset_complete.html')
-    assert 'Django Administration' not in response.content.decode('utf-8')
+    response = client.post(
+        url_from_email,
+        {"new_password1": "asdf", "new_password2": "asdf"},
+        follow=True,
+    )
+    assert template_used(
+        response, "ep19/bs/accounts/password_reset_complete.html"
+    )
+    assert "Django Administration" not in response.content.decode("utf-8")
