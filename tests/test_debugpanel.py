7ba3acd209a6d458d17cd9120ea5c186c5df16df
Artur Czepiel <czepiel.artur@gmail.com>
2019-04-16 21:30:56 +0200

    fix build and oauth

--- tests/test_debugpanel.py
+++ tests/test_debugpanel.py
@@ -1,4 +1,5 @@
-# coding: utf-8
+from django.conf import settings
+from conference.models import Conference
 
 
 def test_index(admin_client):
@@ -7,6 +8,10 @@ def test_index(admin_client):
     """
     url = "/nothing-to-see-here/"
     HTTP_OK_200 = 200
+    Conference.objects.create(
+        code=settings.CONFERENCE_CONFERENCE,
+        name=settings.CONFERENCE_CONFERENCE,
+    )
 
     respnse = admin_client.get(url)
     assert respnse.status_code == HTTP_OK_200
