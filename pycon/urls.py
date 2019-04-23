4b306da03564783d8ba9a4df061c72b65e4ace6f
Artur Czepiel <czepiel.artur@gmail.com>
2019-04-23 21:31:43 +0200

    disable i18n in cms urls

--- pycon/urls.py
+++ pycon/urls.py
@@ -2,7 +2,6 @@
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.views import defaults, static
 
@@ -72,9 +71,9 @@ if settings.DEBUG:
         url(r'^400/$', defaults.bad_request),
     ]
 
-urlpatterns += i18n_patterns(
+urlpatterns += [
     url(r'^', include('cms.urls')),
-)
+]
 
 if hasattr(settings, 'ROSETTA_AFTER_SAVE'):
     # XXX this code would be better in settings.py, unfortunately there
