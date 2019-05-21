f27c657fbe9a81339697038e12aa17c9415ddabe
Artur Czepiel <czepiel.artur@gmail.com>
2019-05-21 21:39:16 +0200

    fix missing __regex

--- conference/tickets.py
+++ conference/tickets.py
@@ -20,16 +20,16 @@ def assign_ticket_to_user(ticket, user):
 
 
 # TODO: Move this somewhere else. Settings maybe(?)
-DEFAULT_SHIRT_SIZE = 'l'
-DEFAULT_DIET = 'omnivorous'
+DEFAULT_SHIRT_SIZE = "l"
+DEFAULT_DIET = "omnivorous"
 
 
 def reset_ticket_settings(ticket):
     tc = ticket.p3_conference
     tc.shirt_size = DEFAULT_SHIRT_SIZE
     tc.diet = DEFAULT_DIET
-    tc.tagline = ''
-    tc.days = ''
+    tc.tagline = ""
+    tc.days = ""
     tc.save()
     return tc
 
@@ -40,7 +40,15 @@ def count_number_of_sold_training_tickets_including_combined_tickets():
         frozen=False,
         orderitem__order___complete=True,
     ).filter(
-        Q(fare__code__regex=FARE_CODE_REGEXES["variants"][FARE_CODE_VARIANTS.TRAINING])
-        | Q(fare__code=FARE_CODE_REGEXES["variants"][FARE_CODE_VARIANTS.COMBINED])
+        Q(
+            fare__code__regex=FARE_CODE_REGEXES["variants"][
+                FARE_CODE_VARIANTS.TRAINING
+            ]
+        )
+        | Q(
+            fare__code__regex=FARE_CODE_REGEXES["variants"][
+                FARE_CODE_VARIANTS.COMBINED
+            ]
+        )
     )
     return qs.count()
