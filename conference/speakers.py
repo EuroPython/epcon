2a8304d6b67f4afcb938843a6bdee257e4da2619
umgelurgel <umgelurgel@gmail.com>
2019-05-21 12:40:46 +0200

    Add permission checks to the profile access.

--- conference/speakers.py
+++ conference/speakers.py
@@ -3,14 +3,21 @@ from django.conf.urls import url
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 
-from conference.models import AttendeeProfile
+from conference.models import AttendeeProfile, Conference, Talk, ATTENDEEPROFILE_VISIBILITY
+from conference.talk_voting import is_user_allowed_to_vote
 
 
 def speaker(request, speaker_slug):
     """
-    Display Talk
+    Display speaker details.
     """
     speaker_profile = get_object_or_404(AttendeeProfile, slug=speaker_slug)
+
+    if not speaker_page_visible(profile=speaker_profile, for_user=request.user):
+        return TemplateResponse(
+            request,
+            "ep19/bs/speakers/speaker_unavailable.html",
+        )
 
     return TemplateResponse(
         request,
@@ -29,6 +36,32 @@ def speaker(request, speaker_slug):
             "speaker_avatar": speaker_profile.p3_profile.public_profile_image_url,
         },
     )
+
+
+def speaker_page_visible(profile, for_user):
+    """
+    Page should be accessible if:
+    * voting is open and current user can vote
+    * or profile has an accepted talk in current conference
+    * or the profile is set as public
+    * or profile is set as visible to participants and current user is authenticated
+    """
+    conference = Conference.objects.current()
+
+    if profile.visibility == ATTENDEEPROFILE_VISIBILITY.PUBLIC:
+        return True
+
+    if for_user.is_authenticated:
+        if conference.voting() and is_user_allowed_to_vote(for_user):
+            return True
+
+        if Talk.objects.filter(conference=conference.code, speakers__user__in=[profile.user]).exists():
+            return True
+
+        if profile.visibility == ATTENDEEPROFILE_VISIBILITY.PARTICIPANTS_ONLY:
+            return True
+
+    return False
 
 
 urlpatterns = [url(r"^(?P<speaker_slug>[\w-]+)/$", speaker, name="speaker")]
