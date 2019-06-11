644041571afa120dc673a2d7f9cb397cd391c2f8
umgelurgel <umgelurgel@users.noreply.github.com>
2019-06-11 17:47:06 +0200

    Assign tickets using case insensitive email. (#1025)
    * Assign tickets using case insensitive email.
    
    * Add handling of assigning tickets to inactive accounts.
    
    * Address review comments.
    
    * Update comments in testing tools.

--- conference/user_panel.py
+++ conference/user_panel.py
@@ -181,7 +181,10 @@ class AssignTicketForm(forms.Form):
         return self.cleaned_data["email"]
 
     def get_user(self):
-        return User.objects.get(email=self.cleaned_data["email"])
+        return User.objects.get(
+            is_active=True,
+            email__iexact=self.cleaned_data["email"],
+        )
 
 
 class CommaStringMultipleChoiceField(forms.MultipleChoiceField):
