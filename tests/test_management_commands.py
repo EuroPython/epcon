5056e43d1f9a25077c123197752728d79e656885
Alexandre M. S <alexsavio@users.noreply.github.com>
2019-06-23 20:59:15 +0200

    fix: fix p3 management commands for badges (#1048)
    * fix: fix p3 management commands for badges
    
    * Small tweaks following python 2to3 upgrade.
    
    Adding xfailing management commands tests.
    
    * fix(p3):  rm pypower from ticket_profiles command

---
+++ tests/test_management_commands.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.xfail
+def test_talk_abstracts():
+    assert False
+
+
+@pytest.mark.xfail
+def test_ticket_profiles():
+    assert False
+
+
+@pytest.mark.xfail
+def test_speakers_list():
+    assert False
+
+
+@pytest.mark.xfail
+def test_speakers_csv():
+    assert False
