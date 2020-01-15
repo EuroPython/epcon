2fb6a342547ab5544dc9e0c7a930bd661200ef50
umgelurgel <umgelurgel@gmail.com>
2020-01-15 20:12:15 +0100

    Remove more old views.

--- conference/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
-
-
-def talk(request, slug):
-    return redirect('talks:talk', permanent=True, talk_slug=slug)
