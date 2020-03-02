a90d1c4447b10e5dc381c1d872eb817053062382
Angel Ramboi <angel@ramboi.com>
2020-03-02 09:24:51 +0100

    Optimize the migration script

--- scripts/django2_migrate.sh
+++ scripts/django2_migrate.sh
@@ -5,10 +5,21 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Clean test accounts and dump incompatible data
 cat ./scripts/django2_migration.sql | sqlite3 $1
 
+# Migrate the latest django DB changes
 ./manage.py migrate
 
+# Import the previously dumped data
+sed -i '1i BEGIN TRANSACTION;' /tmp/assopy_invoice.sql
+echo 'END TRANSACTION;' >> /tmp/assopy_invoice.sql
 cat /tmp/assopy_invoice.sql | sqlite3 $1
+
+sed -i '1i BEGIN TRANSACTION;' /tmp/conference_vototalk.sql
+echo 'END TRANSACTION;' >> /tmp/conference_vototalk.sql
 cat /tmp/conference_vototalk.sql | sqlite3 $1
+
+sed -i '1i BEGIN TRANSACTION;' /tmp/conference_event.sql
+echo 'END TRANSACTION;' >> /tmp/conference_event.sql
 cat /tmp/conference_event.sql | sqlite3 $1
--- scripts/django2_migration.sql
+++ scripts/django2_migration.sql
@@ -1,5 +1,6 @@
 -- These should be applied before running the `migrate` managemen command
 -- A few test accounts where the user was (manually?) deleted from `auth_user`
+BEGIN TRANSACTION;
 DELETE FROM `social_auth_usersocialauth` WHERE id=854;
 DELETE FROM `conference_attendeeprofile` WHERE user_id=1880;
 DELETE FROM `conference_attendeeprofile` WHERE user_id=1896;
@@ -7,7 +8,9 @@ DELETE FROM `conference_attendeeprofile` WHERE user_id=2097;
 DELETE FROM `p3_p3profile` WHERE profile_id=1880;
 DELETE FROM `p3_p3profile` WHERE profile_id=1896;
 DELETE FROM `p3_p3profile` WHERE profile_id=2097;
+END TRANSACTION;
 
+BEGIN TRANSACTION;
 -- Data for the below tables causes issues for the Django migrations
 -- We dump the data and then reimport it after the migrations are done
 
@@ -16,18 +19,19 @@ DELETE FROM `p3_p3profile` WHERE profile_id=2097;
 .out /tmp/conference_vototalk.sql
 SELECT * FROM conference_vototalk;
 
-DELETE FROM `conference_vototalk` WHERE 1;
-
 -- Export data for assopy_invoice and delete data before migration
 .mode insert assopy_invoice
 .out /tmp/assopy_invoice.sql
 SELECT * FROM assopy_invoice;
 
-DELETE FROM `assopy_invoice` WHERE 1;
-
 -- Export data for conference_event and delete data before migration
 .mode insert conference_event
 .out /tmp/conference_event.sql
 SELECT * FROM conference_event;
+END TRANSACTION;
 
+BEGIN TRANSACTION;
+DELETE FROM `conference_vototalk` WHERE 1;
 DELETE FROM `conference_event` WHERE 1;
+DELETE FROM `assopy_invoice` WHERE 1;
+END TRANSACTION;
