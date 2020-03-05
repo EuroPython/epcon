9d293f9ece867fc4ab6822579af1f7e97d880838
umgelurgel <umgelurgel@gmail.com>
2020-03-06 00:17:13 +0100

    Fix tests.

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -693,6 +693,7 @@ def test_cart_fourth_step_renders_correctly(db, user_client):
     assert template_used(response, "conference/cart/step_4_payment.html")
 
 
+@mark.skip(reason='Disabled invoice generation; remove the skip when it is re-eanbled')
 def test_cart_payment_with_zero_total(db, user_client):
     _, fares = setup_conference_with_typical_fares()
     coupon = CouponFactory(user=user_client.user.assopy_user, value='100%')
@@ -711,6 +712,7 @@ def test_cart_payment_with_zero_total(db, user_client):
     assert email_sent_with_subject(ORDER_CONFIRMATION_EMAIL_SUBJECT)
 
 
+@mark.skip(reason='Disabled invoice generation; remove the skip when it is re-eanbled')
 @mock.patch('conference.cart.prepare_for_payment')
 @mock.patch('conference.cart.verify_payment')
 def test_cart_payment_with_non_zero_total(mock_prepare_for_payment, mock_verify_payment, db, user_client):
