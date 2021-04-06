88a362e9adc47546db19058f4c360d15279c08d1
Keith Gaughan <k@stereochro.me>
2021-04-06 18:22:22 +0100

    Re-enable test disabled in #1348 (706617e)

--- tests/test_provisioning_script.py
+++ tests/test_provisioning_script.py
@@ -4,8 +4,6 @@ from django.core.management import call_command
 from pytest import mark
 
 
-# XXX This will have to be investigated further, but not now...
-@mark.skip(reason='Getting a weird error from the script')
 @mark.django_db
 def test_basic_create_initial_data_for_dev_run():
 
