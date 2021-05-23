34bef7e355ffa06351e8a334edb14bc24bcee7a0
Jens Diemer <github.com@jensdiemer.de>
2021-05-23 17:04:11 +0200

    Add a Django-CMS plugin for template code #1414 (#1415)
    Add a minimal test case: Check if html is not escaped and Django template can be used.

--- cms_utils/cms_plugins.py
+++ cms_utils/cms_plugins.py
@@ -1,14 +1,19 @@
-# -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from .models import MarkitUpPluginModel
+from django.utils.translation import ugettext_lazy as _
+
+from .models import MarkitUpPluginModel, TemplatePluginModel
 
 
+@plugin_pool.register_plugin
 class MarkItUpPlugin(CMSPluginBase):
     name = _('MarkItUp')
     model = MarkitUpPluginModel
     render_template = 'djangocms_markitup/markitup.html'
 
 
-plugin_pool.register_plugin(MarkItUpPlugin)
+@plugin_pool.register_plugin
+class TemplatePlugin(CMSPluginBase):
+    name = _('Template Plugin')
+    model = TemplatePluginModel
+    render_template = 'djangocms_template/template_plugin.html'
