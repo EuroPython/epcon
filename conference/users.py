0b15b7e7d86b399198bd337e0e86cb089b013c17
Marc-Andre Lemburg <mal@lemburg.com>
2021-07-07 13:43:46 +0200

    Fix user name generation to always have the names start with letters (#1449)

--- conference/users.py
+++ conference/users.py
@@ -5,9 +5,14 @@ It's a good place to put validators and user management functions/classes
 import os
 
 
-RANDOM_USERNAME_LENGTH = 10
+RANDOM_USERNAME_LENGTH = 12
 
 
 def generate_random_username():
-    """Returns random username of length set by RANDOM_USERNAME_LENGTH"""
-    return os.urandom(100).hex()[:RANDOM_USERNAME_LENGTH]
+    """Returns random username of length set by RANDOM_USERNAME_LENGTH
+    
+       The generated names will always start with 'ep' to make sure that we
+       don't generate numeric only names.
+       
+    """
+    return 'ep' + os.urandom(100).hex()[:RANDOM_USERNAME_LENGTH-2]
